9d4fa2fc9a46392c2c2a147a6ebd98aa428f0c96
Ikorin24 <sunshinexxab@yahoo.co.jp>
2019-03-20 06:11:09 +0900

    ElffyResourcesプロジェクトを追加

---
+++ src/ElffyResource/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElffyResource
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
