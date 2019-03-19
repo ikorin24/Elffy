a75d783a27c41fe3cbf7d1a69ba9ec0faf6e4bf3
Ikorin24 <sunshinexxab@yahoo.co.jp>
2019-03-20 06:40:47 +0900

    コマンドライン引数の処理を追加

--- src/ElffyResource/Program.cs
+++ src/ElffyResource/Program.cs
@@ -4,9 +4,35 @@ namespace ElffyResource
 {
     class MainClass
     {
+        const string HELP = 
+@"How to use:
+    -b | build a resource file
+    -d | decompres a resource file to current directory
+    -h | show help
+";
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(args.Length > 1) {
+                switch(args[0]) {
+                    case "-b": {
+                        var manager = new ResourceManager();
+                        return;
+                    }
+                    case "-d": {
+                        var manager = new ResourceManager();
+                        return;
+                    }
+                    case "-h": {
+                        Console.WriteLine(HELP);
+                        return;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine($"Invalid Usege.{Environment.NewLine}{HELP}");
         }
     }
 }
---
+++ src/ElffyResource/ResourceManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ElffyResource
+{
+    public class ResourceManager
+    {
+        private const string RESOURCE_DIR_NAME = "Resources";
+        private const string OUTPUT_FILE_NAME = "Res.dat";
+
+        public bool Build()
+        {
+            return true;
+        }
+
+        public bool Decompress()
+        {
+            return true;
+        }
+    }
+}
