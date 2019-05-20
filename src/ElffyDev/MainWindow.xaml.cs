2eabe04b47f84195e20270b93a2127c1c437ef1f
Ikorin24 <sunshinexxab@yahoo.co.jp>
2019-05-21 01:07:36 +0900

    MenuのStyleを設定

--- src/ElffyDev/MainWindow.xaml.cs
+++ src/ElffyDev/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ElffyDev
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
