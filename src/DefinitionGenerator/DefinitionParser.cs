a6dbd8d413348d8aa52bcf7e92917862dcabf027
ikorin24 <sunshinexxab@yahoo.co.jp>
2019-09-19 17:38:16 +0900

    Revert "文字列からのクラスインスタンス生成方法を変更"
    This reverts commit 040a5f15ee7aea873e0f67509d96762ea7204584.

--- src/DefinitionGenerator/DefinitionParser.cs
+++ src/DefinitionGenerator/DefinitionParser.cs
@@ -81,7 +81,7 @@ namespace DefinitionGenerator
                 return element.Attributes()
                               .Where(a => !MetaData.IsMetaDataAttribute(a))
                               .Select(a => (PropName: a.Name.LocalName, Value: a.Value))
-                              .Select(p => $"{Definition.ValueConverterMethodName}(\"{p.Value}\", __ => {variable.Name} = __);");
+                              .Select(p => $"{variable.Name}.{p.PropName} = {variable.Name}.{p.PropName}.{nameof(DefinitionExtension.FromAltString)}(\"{p.Value}\");");
             })
             .Concat(
                 elements.Skip(1)
