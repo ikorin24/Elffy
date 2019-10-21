5f8f0a11bf0f0550265c0bc8ed0aa92a877445b2
ikorin24 <sunshinexxab@yahoo.co.jp>
2019-10-22 08:44:57 +0900

    同期コンテキストを掴まない Task を開始するためのクラスを追加

---
+++ src/Elffy/Threading/NonCaptureContextTaskFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Elffy.Threading
+{
+    internal static class NonCaptureContextTaskFactory
+    {
+
+        public static Task StartNew(Action action)
+        {
+            var task = Task.Factory.StartNew(action);
+            task.ConfigureAwait(false);
+            return task;
+        }
+
+        public static Task StartNew(Action action, CancellationToken cancellationToken)
+        {
+            var task = Task.Factory.StartNew(action, cancellationToken);
+            task.ConfigureAwait(false);
+            return task;
+        }
+
+        public static Task<T> StartNew<T>(Func<T> func)
+        {
+            var task = Task.Factory.StartNew(func);
+            task.ConfigureAwait(false);
+            return task;
+        }
+
+        public static Task<T> StartNew<T>(Func<T> func, CancellationToken cancellationToken)
+        {
+            var task = Task.Factory.StartNew(func, cancellationToken);
+            task.ConfigureAwait(false);
+            return task;
+        }
+    }
+}
