080ea220533da8a1c4121e0dfaf6baa5ffac2b57
ikorin24 <sunshinexxab@yahoo.co.jp>
2019-10-24 09:59:52 +0900

    テクスチャのY軸を反転させるコンストラクタを追加

--- src/Elffy/Shape/Plain.cs
+++ src/Elffy/Shape/Plain.cs
@@ -13,6 +13,7 @@ using System.Runtime.InteropServices;
 
 namespace Elffy.Shape
 {
+    /// <summary>正方形の平面の3Dオブジェクトクラス</summary>
     public class Plain : Renderable
     {
         private static readonly Vertex[] _vertexArray = new Vertex[4]
@@ -22,12 +23,35 @@ namespace Elffy.Shape
             new Vertex(new Vector3(1, -1, 0),  new Vector3(0, 0, 1), new Vector2(1, 0)),
             new Vertex(new Vector3(1, 1, 0),   new Vector3(0, 0, 1), new Vector2(1, 1)),
         };
+        private static readonly Vertex[] _inverseTexCoordYVertexArray = new Vertex[4]
+        {
+            new Vertex(new Vector3(-1, 1, 0),  new Vector3(0, 0, 1), new Vector2(0, 0)),
+            new Vertex(new Vector3(-1, -1, 0), new Vector3(0, 0, 1), new Vector2(0, 1)),
+            new Vertex(new Vector3(1, -1, 0),  new Vector3(0, 0, 1), new Vector2(1, 1)),
+            new Vertex(new Vector3(1, 1, 0),   new Vector3(0, 0, 1), new Vector2(1, 0)),
+        };
+
         private static readonly int[] _indexArray = new int[6] { 0, 1, 2, 2, 3, 0 };
+
+        private bool _isTexCoordYInversed;
+
+        /// <summary>平面の3Dオブジェクトを生成します</summary>
+        public Plain()
+        {
+        }
+
+        /// <summary>平面の3Dオブジェクトを生成します</summary>
+        /// <param name="isTexCoordYInversed">テクスチャのY軸方向を反転させる場合 true</param>
+        public Plain(bool isTexCoordYInversed)
+        {
+            _isTexCoordYInversed = isTexCoordYInversed;
+        }
 
         protected override void OnActivated()
         {
             base.OnActivated();
-            InitGraphicBuffer(_vertexArray, _indexArray);
+            var vertexArray = _isTexCoordYInversed ? _inverseTexCoordYVertexArray : _vertexArray;
+            InitGraphicBuffer(vertexArray, _indexArray);
         }
     }
 }
