9e9b1a397fdac8f41af8027a95294e033c08d5c6
ikorin24 <sunshinexxab@yahoo.co.jp>
2019-10-31 07:05:05 +0900

    ComponentOwner のパフォーマンス改善

--- src/Elffy/Core/ComponentOwner.cs
+++ src/Elffy/Core/ComponentOwner.cs
@@ -1,6 +1,7 @@
 ﻿using Elffy.Exceptions;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Elffy.Core
 {
@@ -9,64 +10,136 @@ namespace Elffy.Core
     /// </summary>
     public abstract class ComponentOwner : FrameObject
     {
-        private List<object> _components = new List<object>();
-        private List<Type> _types = new List<Type>();
-
-        #region GetComponent
         /// <summary>Get component of specified type</summary>
         /// <typeparam name="T">component type</typeparam>
         /// <returns>component object</returns>
-        public T GetComponent<T>()
+        public T GetComponent<T>() where T : class
         {
-            return (T)_components.Find(x => x is T);
+            if(!ComponentStore<T>.HasComponentOf(this)) { throw new InvalidOperationException($"No component of type '{typeof(T).FullName}'"); }
+            return ComponentStore<T>.Get(this);
         }
-        #endregion
 
-        #region AddComponent
+        /// <summary>Get component of specified type. If no component whose type is <typeparamref name="T"/>, return default(<typeparamref name="T"/>)</summary>
+        /// <typeparam name="T">component type</typeparam>
+        /// <returns>component of type <typeparamref name="T"/>. If no component, default(<typeparamref name="T"/>)</returns>
+        public T GetComponentOrDefault<T>() where T : class
+        {
+            return ComponentStore<T>.GetOrDefault(this);
+        }
+
         /// <summary>Add component object</summary>
         /// <typeparam name="T">component type</typeparam>
         /// <param name="component">component object</param>
-        public void AddComponent<T>(T component)
+        public void AddComponent<T>(T component) where T : class
         {
             ArgumentChecker.ThrowIfNullArg(component, nameof(component));
-            var type = typeof(T);
-            ArgumentChecker.ThrowIf(_types.Contains(type), new ArgumentException($"Component type '{type.FullName}' already exists."));
-
-            // _types と _component は同じ index は同じ要素に対応している前提
-            // 同じ型の Component は複数存在しない前提
-
-            _types.Add(type);
-            _components.Add(component);
+            ArgumentChecker.ThrowIf(ComponentStore<T>.HasComponentOf(this), new ArgumentException($"Component type '{typeof(T).FullName}' already exists."));
+            ComponentStore<T>.Add(this, component);
         }
-        #endregion
 
-        #region HasComponent
+        /// <summary>Add or replace component whose type is <typeparamref name="T"/>. Return true if replaced, otherwize false.</summary>
+        /// <typeparam name="T">component type</typeparam>
+        /// <param name="component">the component</param>
+        /// <returns>Return true if replaced, otherwize false.</returns>
+        public bool AddOrReplaceComponent<T>(T component) where T : class
+        {
+            ArgumentChecker.ThrowIfNullArg(component, nameof(component));
+            return ComponentStore<T>.AddOrReplace(this, component);
+        }
+
         /// <summary>Get whether <see cref="ComponentOwner"/> has a component of specified type</summary>
         /// <typeparam name="T">component type</typeparam>
         /// <returns>True if <see cref="ComponentOwner"/> has the component; otherwise, false</returns>
-        public bool HasComponent<T>()
+        public bool HasComponent<T>() where T : class
         {
-            return _types.Contains(typeof(T));
+            return ComponentStore<T>.HasComponentOf(this);
         }
-        #endregion
 
-        #region RemoveComponent
         /// <summary>Remove the component of specified type.</summary>
         /// <typeparam name="T">component type</typeparam>
         /// <returns>True if the component is removed. False if the component does not exist.</returns>
-        public bool RemoveComponent<T>()
+        public bool RemoveComponent<T>() where T : class
         {
-            // _types と _component は同じ index は同じ要素に対応している前提
-            // 同じ型の Component は複数存在しない前提
-            for(int i = 0; i < _types.Count; i++) {
-                if(_types[i] == typeof(T)) {
-                    _types.RemoveAt(i);
-                    _components.RemoveAt(i);
+            return ComponentStore<T>.Remove(this);
+        }
+
+
+        /// <summary>
+        /// Component store of type <typeparamref name="T"/>.<para/>
+        /// The reference to an instance of dictionary key is weak refarence.<para/>
+        /// </summary>
+        /// <typeparam name="T">component type</typeparam>
+        private static class ComponentStore<T> where T : class
+        {
+            /// <summary>weak reference dictionary of (owner, component) pair.</summary>
+            private static readonly ConditionalWeakTable<ComponentOwner, T> _components = new ConditionalWeakTable<ComponentOwner, T>();
+
+            /// <summary>Get whether specified owner has component whose type is <typeparamref name="T"/>.</summary>
+            /// <param name="owner">the owner of the component</param>
+            /// <returns>Return true if the owner has component of <typeparamref name="T"/>, otherwize false.</returns>
+            public static bool HasComponentOf(ComponentOwner owner)
+            {
+                return _components.TryGetValue(owner, out _);
+            }
+
+            /// <summary>Add component whose type is <typeparamref name="T"/></summary>
+            /// <param name="owner">the owner of the component</param>
+            /// <param name="component">the component</param>
+            public static void Add(ComponentOwner owner, T component)
+            {
+                _components.Add(owner, component);
+            }
+
+            /// <summary>Add or replace component whose type is <typeparamref name="T"/>. Return true if replaced, otherwize false.</summary>
+            /// <param name="owner">the owner of the component</param>
+            /// <param name="component">the component</param>
+            /// <returns>Return true if replaced, otherwize false.</returns>
+            public static bool AddOrReplace(ComponentOwner owner, T component)
+            {
+                if(_components.TryGetValue(owner, out _)) {
+                    _components.Remove(owner);
+                    _components.Add(owner, component);
                     return true;
                 }
+                else {
+                    _components.Add(owner, component);
+                    return false;
+                }
             }
-            return false;
+
+            /// <summary>Get component of specified owner.</summary>
+            /// <param name="owner">the owner of the component</param>
+            /// <returns>the component</returns>
+            public static T Get(ComponentOwner owner)
+            {
+                if(_components.TryGetValue(owner, out var component)) {
+                    return component;
+                }
+                else {
+                    throw new KeyNotFoundException();
+                }
+            }
+
+            /// <summary>Get component of specified owner. If not exist, return default</summary>
+            /// <param name="owner">the owner of the component.</param>
+            /// <returns>the component</returns>
+            public static T GetOrDefault(ComponentOwner owner)
+            {
+                if(_components.TryGetValue(owner, out var component)) {
+                    return component;
+                }
+                else {
+                    return default;
+                }
+            }
+
+            /// <summary>Remove component of specified owner.</summary>
+            /// <param name="owner">the owner of the component</param>
+            /// <returns>Return true if removed. Return false when the owner has no component.</returns>
+            public static bool Remove(ComponentOwner owner)
+            {
+                return _components.Remove(owner);
+            }
         }
-        #endregion
     }
 }
--- src/Test/ComponentOwnerTest.cs
+++ src/Test/ComponentOwnerTest.cs
@@ -17,40 +17,73 @@ namespace Test
             Assert.IsFalse(owner.HasComponent<Component1>());
             Assert.IsFalse(owner.HasComponent<Component2>());
 
+            // owner's component == [ ]
+
             Assert.IsFalse(owner.RemoveComponent<Component1>());
             Assert.IsFalse(owner.RemoveComponent<Component2>());
+
+            // owner's component == [ ]
 
             Assert.IsFalse(owner.HasComponent<Component1>());
             Assert.IsFalse(owner.HasComponent<Component2>());
 
+            // owner's component == [ ]
+
             owner.AddComponent(comp1);
             Assert.IsTrue(owner.HasComponent<Component1>());
             Assert.IsFalse(owner.HasComponent<Component2>());
+
+            // owner's component == [ comp1{ Name = "hoge" } ]
 
             Assert.IsTrue(owner.RemoveComponent<Component1>());
             Assert.IsFalse(owner.HasComponent<Component1>());
             Assert.IsFalse(owner.HasComponent<Component2>());
 
+            // owner's component == [ ]
+
             owner.AddComponent(comp2);
             Assert.IsFalse(owner.HasComponent<Component1>());
             Assert.IsTrue(owner.HasComponent<Component2>());
+
+            // owner's component == [ comp2{ Name = "piyo" } ]
 
             owner.AddComponent(comp1);
             Assert.IsTrue(owner.HasComponent<Component1>());
             Assert.IsTrue(owner.HasComponent<Component2>());
 
+            // owner's component == [ comp1{ Name = "hoge"}, comp2{ Name = "piyo" } ]
+
             Assert.ThrowsException<ArgumentException>(() => owner.AddComponent(new Component2()));
+
+            // owner's component == [ comp1{ Name = "hoge"}, comp2{ Name = "piyo" } ]
 
             var c1 = owner.GetComponent<Component1>();
             Assert.IsNotNull(c1);
             Assert.IsTrue(c1.Name == "hoge");
 
+            // owner's component == [ comp1{ Name = "hoge"}, comp2{ Name = "piyo" } ]
+
             var c2 = owner.GetComponent<Component2>();
             Assert.IsNotNull(c2);
             Assert.IsTrue(c2.Name == "piyo");
 
+            // owner's component == [ comp1{ Name = "hoge"}, comp2{ Name = "piyo" } ]
+
             Assert.IsFalse(owner.HasComponent<Component3>());
-            Assert.IsNull(owner.GetComponent<Component3>());
+            Assert.ThrowsException<InvalidOperationException>(() => owner.GetComponent<Component3>());
+
+            Assert.IsTrue(owner.AddOrReplaceComponent(new Component1() { Name = "foo" }));
+
+            // owner's component == [ comp1{ Name = "foo"}, comp2{ Name = "piyo" } ]
+
+            Assert.IsFalse(owner.AddOrReplaceComponent(new Component3() { Name = "bar" }));
+            Assert.IsTrue(owner.GetComponent<Component1>().Name == "foo");
+            Assert.IsTrue(owner.GetComponent<Component3>().Name == "bar");
+
+            // owner's component == [ comp1{ Name = "foo"}, comp2{ Name = "piyo" }, comp3{ Name = "bar" } ]
+
+            Assert.IsTrue(owner.RemoveComponent<Component2>());
+            Assert.IsNull(owner.GetComponentOrDefault<Component2>());
         }
 
         class Component1
@@ -65,6 +98,7 @@ namespace Test
 
         class Component3
         {
+            public string Name { get; set; }
         }
 
         class SampleOwner : ComponentOwner
