9f374b68c98077bd86852931db863cd398ba5d5b
ikorin24 <sunshinexxab@yahoo.co.jp>
2019-12-09 15:15:39 +0900

    add glcontrol test project

---
+++ src/GLControlTest/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace GLControlTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl = new Elffy.Platforms.Windows.GLControl();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl.Location = new System.Drawing.Point(0, 0);
+            this.glControl.Name = "glControl1";
+            this.glControl.Size = new System.Drawing.Size(377, 335);
+            this.glControl.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.glControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Elffy.Platforms.Windows.GLControl glControl;
+    }
+}
+
---
+++ src/GLControlTest/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace GLControlTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            glControl.Load += GlControl1_Load;
+            glControl.Resize += GlControl1_Resize;
+            glControl.Paint += GlControl1_Paint;
+        }
+
+        private void GlControl1_Paint(object sender, PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            Matrix4 modelview = Matrix4.LookAt(Vector3.Zero, Vector3.UnitZ, Vector3.UnitY);
+            GL.LoadMatrix(ref modelview);
+
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.Color4(Color4.White);
+            GL.Vertex3(-1.0f, 1.0f, 4.0f);
+            GL.Color4(Color4.Red);
+            GL.Vertex3(-1.0f, -1.0f, 4.0f);
+            GL.Color4(Color4.Lime);
+            GL.Vertex3(1.0f, -1.0f, 4.0f);
+            GL.Color4(Color4.Blue);
+            GL.Vertex3(1.0f, 1.0f, 4.0f);
+
+            GL.End();
+            glControl.SwapBuffers();
+            //glControl.Invalidate();
+        }
+
+        private void GlControl1_Resize(object sender, EventArgs e)
+        {
+            GL.Viewport(0, 0, glControl.Size.Width, glControl.Size.Height);
+            GL.MatrixMode(MatrixMode.Projection);
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, (float)glControl.Size.Width / (float)glControl.Size.Height, 1.0f, 64.0f);
+            GL.LoadMatrix(ref projection);
+        }
+
+        private void GlControl1_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(Color.Red);
+            GL.Enable(EnableCap.DepthTest);
+        }
+    }
+}
