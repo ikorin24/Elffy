dc8c01be2e5e3b4bcc8257ad330f3451a125593e
ikorin24 <sunshinexxab@yahoo.co.jp>
2019-12-21 03:56:52 +0900

    不要なメソッドを削除

--- src/Elffy/Elffy/Engine.cs
+++ src/Elffy/Elffy/Engine.cs
@@ -14,37 +14,13 @@ namespace Elffy
     {
         public static bool IsRunning { get; private set; }
 
-        public static IHostScreen CurrentScreen { get; private set; } = null!;
-
-        public static void SingleScreenRun(int width, int height, string title, WindowStyle windowStyle, YAxisDirection uiYAxisDirection, ActionEventHandler<IHostScreen> initialized)
-            => SingleScreenRun(width, height, title, windowStyle, uiYAxisDirection, null!, initialized);
-
-        public static void SingleScreenRun(int width, int height, string title, WindowStyle windowStyle, 
-                                           YAxisDirection uiYAxisDirection, string icon, ActionEventHandler<IHostScreen> initialized)
+        /// <summary>Get or set current <see cref="IHostScreen"/>. This is current target screen of rendering.</summary>
+        public static IHostScreen CurrentScreen
         {
-            ArgumentChecker.ThrowOutOfRangeIf(width < 0, nameof(width), width, $"{nameof(width)} is out of range");
-            ArgumentChecker.ThrowOutOfRangeIf(height < 0, nameof(height), height, $"{nameof(height)} is out of range");
-            if(IsRunning) { throw new InvalidOperationException("Engine is already runnning."); }
-            IsRunning = true;
-            Resources.Initialize();
-            try {
-                var gameScreen = Platform.PlatformType switch
-                {
-                    PlatformType.Windows => GetWindowGameScreen(width, height, title, windowStyle, uiYAxisDirection, icon),
-                    PlatformType.MacOSX => GetWindowGameScreen(width, height, title, windowStyle, uiYAxisDirection, icon),
-                    PlatformType.Unix => GetWindowGameScreen(width, height, title, windowStyle, uiYAxisDirection, icon),
-                    _ => throw Platform.PlatformNotSupported()
-                };
-                gameScreen.TargetRenderPeriod = gameScreen.FrameDelta.TotalSeconds; // TODO: ???
-                gameScreen.Initialized += initialized;
-                Dispatcher.SetMainThreadID();
-                CurrentScreen = gameScreen;
-                gameScreen.Show(width, height, title, null, windowStyle);
-            }
-            finally {
-                CustomSynchronizationContext.Delete();
-            }
+            get => _currentScreen ?? throw new InvalidOperationException($"There is no {nameof(IHostScreen)} instance in {nameof(Engine)}.");
+            private set => _currentScreen = value;
         }
+        private static IHostScreen? _currentScreen = null;
 
         public static void Run()
         {
@@ -57,6 +33,11 @@ namespace Elffy
         /// <summary>Switch current screen to specified screen. This method is for delegate</summary>
         /// <param name="dummy">dummy instance of extension method source. (null is valid because this is not used in this method.)</param>
         /// <param name="screen">screen of switching target</param>
+        /// <remarks>
+        /// This method is called every frame from delegate.
+        /// Static method called from delegate is slower than instance one.
+        /// If Currying by extension method, it is called as faster as method of instance.
+        /// </remarks>
         internal static void SwitchScreen(this CurriedDelegateDummy? dummy, IHostScreen screen)
         {
             CurrentScreen = screen;
@@ -97,19 +78,6 @@ namespace Elffy
             finally {
                 CustomSynchronizationContext.Delete();
             }
-        }
-
-        private static IHostScreen GetWindowGameScreen(int width, int height, string title, WindowStyle windowStyle, YAxisDirection uiYAxisDirection, string? iconResourcePath)
-        {
-            var window = new Window(width, height, title, windowStyle, uiYAxisDirection);
-            if(iconResourcePath != null) {
-                if(Resources.HasResource(iconResourcePath)) {
-                    using(var stream = Resources.GetStream(iconResourcePath)) {
-                        window.Icon = new Icon(stream);
-                    }
-                }
-            }
-            return window;
         }
     }
 }
