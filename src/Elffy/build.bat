a121518d311c0f29de41709205ec2e26cd51bebe
ikorin24 <sunshinexxab@yahoo.co.jp>
2019-12-21 19:11:15 +0900

    nugetからの復元を追加

--- src/Elffy/build.bat
+++ src/Elffy/build.bat
@@ -6,6 +6,8 @@ exec cmd //c ${0//\//\\\\} $*
 :WINDOWS
 
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration=Release Elffy.csproj
+set msbuild_path="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+%msbuild_path% -t:restore
+%msbuild_path% /p:Configuration=Release Elffy.csproj
 
 
