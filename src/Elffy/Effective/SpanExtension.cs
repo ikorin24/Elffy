7d5e31b773389635591a1396ca4fdbe84306ba1b
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-01-19 09:18:30 +0900

    ReadOnlySpan<T> 用のオーバーロード追加

--- src/Elffy/Effective/SpanExtension.cs
+++ src/Elffy/Effective/SpanExtension.cs
@@ -18,7 +18,24 @@ namespace Elffy.Effective
         /// <param name="source">source <see cref="Span{T}"/></param>
         /// <returns>destination <see cref="Span{T}"/></returns>
         public static Span<TTo> MarshalCast<TFrom, TTo>(this Span<TFrom> source) where TFrom : struct
-                                                                          where TTo : struct
+                                                                                 where TTo : struct
+        {
+            return MemoryMarshal.Cast<TFrom, TTo>(source);
+        }
+
+        /// <summary>
+        /// Cast whole <see cref="ReadOnlySpan{T}"/> to another type of <see cref="ReadOnlySpan{T}"/>.  <para/>
+        /// This is not each-element cast. The memory layout is not changed.                            <para/>
+        /// [example] from <see cref="byte"/> to <see cref="short"/>                                    <para/>
+        /// from : [ 0x00 | 0x01 | 0x02 | 0x03 ] == ReadOnlySpan(byte),  Length=4                       <para/>
+        /// to   : [ 0x00   0x01 | 0x02   0x03 ] == ReadOnlySpan(short), Length=2                       <para/>
+        /// </summary>
+        /// <typeparam name="TFrom">type of source <see cref="ReadOnlySpan{T}"/> item</typeparam>
+        /// <typeparam name="TTo">type of destination <see cref="ReadOnlySpan{T}"/> item</typeparam>
+        /// <param name="source">source <see cref="ReadOnlySpan{T}"/></param>
+        /// <returns>destination <see cref="ReadOnlySpan{T}"/></returns>
+        public static ReadOnlySpan<TTo> MarshalCast<TFrom, TTo>(this ReadOnlySpan<TFrom> source) where TFrom : struct
+                                                                                                 where TTo : struct
         {
             return MemoryMarshal.Cast<TFrom, TTo>(source);
         }
