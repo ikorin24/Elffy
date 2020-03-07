e49877fb5f8fe924fd4a9f74376d1305c08ddc5e
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-03-07 18:50:09 +0900

    コメントを追加

--- src/Elffy/Elffy/CameraMouse.cs
+++ src/Elffy/Elffy/CameraMouse.cs
@@ -44,15 +44,16 @@ namespace Elffy
         {
             var target = Target;
             var p0 = _camera.Position - target;
-            var theta = new Angle(thetaRad);                                    // 水平方向の回転角
-            var phi = new Angle(phiRad);                                        // 垂直方向の回転角
-            var alpha = new Angle(p0.X / p0.Xz.Length, p0.Z / p0.Xz.Length);    // XZ平面への正射影ベクトルとZ軸との角
+            var theta = new Angle(thetaRad);                // 水平方向の回転角
+            var phi = new Angle(phiRad);                    // 垂直方向の回転角
+            var alpha = new Angle(p0.X / p0.Xz.Length, p0.Z / p0.Xz.Length);    // XZ平面への正射影ベクトルとZ軸との角 (経度)
 
+            // 経度0まで
             var matrix = 
-            // 垂直方向回転
+            // 垂直方向回転 (経度0へ水平方向回転 → 垂直方向回転 → 元の経度まで水平方向回転)
                 new Matrix3(alpha.Cos, 0, alpha.Sin,
-                                     0, 1, 0,
-                                     -alpha.Sin, 0, alpha.Cos)
+                            0, 1, 0,
+                            -alpha.Sin, 0, alpha.Cos)
                * new Matrix3(1, 0, 0,
                              0, phi.Cos, -phi.Sin,
                              0, phi.Sin, phi.Cos)
@@ -64,12 +65,8 @@ namespace Elffy
                              0, 1, 0,
                              theta.Sin, 0, theta.Cos);
 
-            // 垂直方向回転
-            var p1 = p0 * matrix;
             _camera.Direction = _camera.Direction * matrix;
-            _camera.Position = target + p1;
-            System.Diagnostics.Debug.WriteLine(_camera.Direction);
-            //_camera.LookAt(target, target + p1);
+            _camera.Position = target + p0 * matrix;
         }
 
         private readonly struct Angle
