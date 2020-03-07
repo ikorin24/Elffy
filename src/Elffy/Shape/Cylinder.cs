1e55c5748b3ff93dc0e5ad97c708aed6ddac5ce0
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-03-08 08:47:58 +0900

    CylinderとSphereのひな型を用意

---
+++ src/Elffy/Shape/Cylinder.cs
@@ -0,0 +1,32 @@
+﻿using Elffy.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elffy.Shape
+{
+    public class Cylinder : Renderable
+    {
+        private static readonly ReadOnlyMemory<Vertex> _vertexArray;
+        private static readonly ReadOnlyMemory<int> _indexArray;
+
+        static Cylinder()
+        {
+            _vertexArray = new ReadOnlyMemory<Vertex>();
+            _indexArray = new ReadOnlyMemory<int>();
+        }
+
+        public Cylinder()
+        {
+            Activated += OnActivated;
+            throw new NotImplementedException();
+        }
+
+        private void OnActivated(FrameObject sender)
+        {
+            LoadGraphicBuffer(_vertexArray.Span, _indexArray.Span);
+        }
+    }
+}
---
+++ src/Elffy/Shape/Sphere.cs
@@ -0,0 +1,34 @@
+﻿#nullable enable
+using Elffy.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elffy.Shape
+{
+    public class Sphere : Renderable
+    {
+        private static readonly ReadOnlyMemory<Vertex> _vertexArray;
+        private static readonly ReadOnlyMemory<int> _indexArray;
+
+        static Sphere()
+        {
+            // TODO:
+            _vertexArray = new ReadOnlyMemory<Vertex>();
+            _indexArray = new ReadOnlyMemory<int>();
+        }
+
+        public Sphere()
+        {
+            Activated += OnActivated;
+            throw new NotImplementedException();
+        }
+
+        private void OnActivated(FrameObject frameObject)
+        {
+            LoadGraphicBuffer(_vertexArray.Span, _indexArray.Span);
+        }
+    }
+}
