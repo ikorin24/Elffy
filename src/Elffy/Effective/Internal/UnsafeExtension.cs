8a0f830eb299889d6fdf2156e5e9aaf9e578ea2c
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-03-11 01:34:09 +0900

    List<T> を Memory<T> へ変換するメソッドを追加

--- src/Elffy/Effective/Internal/UnsafeExtension.cs
+++ src/Elffy/Effective/Internal/UnsafeExtension.cs
@@ -9,74 +9,40 @@ namespace Elffy.Effective.Internal
     internal static class UnsafeExtension
     {
         /// <summary>
-        /// ************** [NOTICE] **************                                                  <para/>
-        /// This is VERY UNSAFE !!!!                                                                <para/>
-        /// The method may depend on version of .NET or mono !                                      <para/>
-        /// See more info in the comment of the method or <see cref="ReadOnlyCollectionDummy{T}"/>. <para/>
-        /// - - - - - - - - - - - - - - - - - - - -                                                 <para/>
-        /// Arg must be a wrapper of T[] or List(T). NOT any other IList(T).                        <para/>
-        /// This is not related to the above restriction.                                           <para/>
+        /// Get <see cref="Span{T}"/> from <see cref="ReadOnlyCollection{T}"/> <para/>
+        /// ************** [NOTICE] **************                             <para/>
+        /// This is VERY UNSAFE !!!!                                           <para/>
         /// </summary>
         /// <typeparam name="T">type of item</typeparam>
         /// <param name="collection"><see cref="ReadOnlyCollection{T}"/></param>
         /// <returns>Inner array of <see cref="ReadOnlyCollection{T}"/></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static Span<T> ExtractInnerArray<T>(this ReadOnlyCollection<T> collection) // TODO: Test this method on each .NET version
         {
             if(collection == null) { throw new ArgumentNullException(nameof(collection)); }
-
-            // **********************************************************
-            // Apply force cast ReadOnlyCollection<T> into dummy.
-            // Layout of the dummy class must be same as ReadOnlyCollection<T>.
-            // If not, the memory becomes crashed and nobody knows what will happen !!
-            // **********************************************************
-
             var dummy = Unsafe.As<ReadOnlyCollectionDummy<T>>(collection);
-
-            // The memory already has crashed here If the above code is invalid.
-            // If it becomes in the case, the following code may fly away.  Oh Nooooooooo  :_(
-
             if(dummy.List is T[] array) {
                 return array.AsSpan();
             }
-            //else if(dummy.List is List<T> list) {
-            //    return list.ExtractInnerArray();
-            //}
+            else if(dummy.List is List<T> list) {
+                return list.AsSpan();
+            }
             else {
                 throw new InvalidCastException($"Inner field of {nameof(ReadOnlyCollection<T>)} cannot be implemented.\r\nType : {dummy.List.GetType()}");
             }
         }
 
-        /*
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static Memory<T> AsMemory<T>(this List<T> list) => new Memory<T>(Unsafe.As<ListDummy<T>>(list).Item, 0, list.Count);
 
-        /// <summary>
-        /// ************** [NOTICE] **************                                                  <para/>
-        /// This is VERY UNSAFE !!!!                                                                <para/>
-        /// The method may depend on version of .NET or mono !                                      <para/>
-        /// See more info in the comment of the method or <see cref="ListDummy{T}{T}"/>.            <para/>
-        /// </summary>
-        /// <typeparam name="T">type of item</typeparam>
-        /// <param name="collection"><see cref="List{T}"/></param>
-        /// <returns>Inner array of <see cref="List{T}"/></returns>
-        internal static Span<T> ExtractInnerArray<T>(this List<T> list)                 // TODO: Test this method on each .NET version
-        {
-            if(list == null) { throw new ArgumentNullException(nameof(list)); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static Span<T> AsSpan<T>(this List<T> list) => list.AsMemory().Span;
 
-            // **********************************************************
-            // Apply force cast List<T> into dummy.
-            // Layout of the dummy class must be same as List<T>.
-            // If not, the memory becomes crashed and nobody knows what will happen !!
-            // **********************************************************
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static ReadOnlyMemory<T> AsReadOnlyMemory<T>(this List<T> list) => new ReadOnlyMemory<T>(Unsafe.As<ListDummy<T>>(list).Item, 0, list.Count);
 
-            var dummy = Unsafe.As<ListDummy<T>>(list);
-
-            // The memory already has crashed here If the above code is invalid.
-            // If it becomes in the case, the following code may fly away.  Oh Nooooooooo  :_(
-
-            var span = dummy.Array.AsSpan(0, dummy.Size);
-            return span;
-        }
-
-        */
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static ReadOnlySpan<T> AsReadOnlySpan<T>(this List<T> list) => list.AsReadOnlyMemory().Span;
 
         /// <summary>
         /// ************** [NOTICE] **************                                                  <para/>
@@ -94,38 +60,10 @@ namespace Elffy.Effective.Internal
         }
 
 
-        // ListDummy は .NET のバージョンに左右されるのが危険すぎる
-        /*
-
-
-        /// <summary>
-        /// ************** [NOTICE] **************                                                  <para/>
-        /// This is VERY UNSAFE !!!!                                                                <para/>
-        /// The method may depend on version of .NET or mono !                                      <para/>
-        /// Layout of this class on the memory MUST be same as <see cref="List{T}"/>                <para/>
-        /// This is a dummy class of <see cref="List{T}"/>                                          <para/>
-        /// </summary>
-        /// <typeparam name="T">item type</typeparam>
-        private class ListDummy<T>
+        private abstract class ListDummy<T>
         {
-#pragma warning disable 0649
-#pragma warning disable 0169
-#pragma warning disable 0414
-            private const int _defaultCapacity = 4;
-            public readonly T[] Array = null!;
-            public readonly int Size;
-            private readonly int _version;
-#if NETFRAMEWORK
-            private object? _syncRoot;
-#endif
-            //private static readonly T[]? _emptyArray = null!;
-#pragma warning restore 0649
-#pragma warning restore 0169
-#pragma warning restore 0414
+            internal T[] Item = default!;
             private ListDummy() { }
         }
-
-        */
-
     }
 }
---
+++ src/Test/UnsafeExtensionTest.cs
@@ -0,0 +1,46 @@
+﻿#nullable enable
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Elffy.Effective.Internal;
+using System.Collections.ObjectModel;
+
+namespace Test
+{
+    [TestClass]
+    public class UnsafeExtensionTest
+    {
+        [TestMethod]
+        public void ListAsReadOnlyMemoryTest()
+        {
+            var list = Enumerable.Range(0, 10).ToList();
+            var i = 0;
+            foreach(var item in list.AsReadOnlyMemory().Span) {
+                Assert.IsTrue(i == item);
+                i++;
+            }
+        }
+
+        [TestMethod]
+        public void ReadOnlyCollectionToSpanTest()
+        {
+            {
+                var readOnlyCollection = Enumerable.Range(0, 10).ToList().AsReadOnly();
+                var i = 0;
+                foreach(var item in readOnlyCollection.ExtractInnerArray()) {
+                    Assert.IsTrue(i == item);
+                    i++;
+                }
+            }
+
+
+            {
+                var readOnlyCollection2 = new ReadOnlyCollection<int>(Enumerable.Range(0, 10).ToArray());
+                var i = 0;
+                foreach(var item in readOnlyCollection2.ExtractInnerArray()) {
+                    Assert.IsTrue(i == item);
+                    i++;
+                }
+            }
+        }
+    }
+}
