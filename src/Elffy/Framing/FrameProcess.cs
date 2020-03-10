1c17a86bff165b87695242256ea98cbc49eb430d
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-03-11 08:41:53 +0900

    コメント修正

--- src/Elffy/Framing/FrameProcess.cs
+++ src/Elffy/Framing/FrameProcess.cs
@@ -10,10 +10,6 @@ namespace Elffy.Framing
     public readonly struct FrameProcess
     {
         /// <summary><see cref="ProcessObj"/> を有効にするレイヤー</summary>
-        /// <remarks>
-        /// TODO: 複数の <see cref="IHostScreen"/> がある場合、その <see cref="IHostScreen.Layers"/> の <see cref="LayerCollection.SystemLayer"/> を使うように
-        /// 実装を変える必要がある
-        /// </remarks>
         internal static readonly SystemLayer TARGET_LAYER = Engine.CurrentScreen.Layers.SystemLayer;
 
         /// <summary>何もしない動作を表す <see cref="FrameProcessBehavior"/> オブジェクト</summary>
