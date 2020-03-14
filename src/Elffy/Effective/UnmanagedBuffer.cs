fcf99f77a3a896f82f181f1ae5695498bcc825bf
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-03-15 04:30:00 +0900

    UnmanagedList<T>を実装

--- src/Elffy/Effective/UnmanagedBuffer.cs
+++ src/Elffy/Effective/UnmanagedBuffer.cs
@@ -3,15 +3,36 @@ using System;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 using Elffy.Exceptions;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Elffy.Effective
 {
     /// <summary>Disposable <see cref="Span{T}"/> of unmanaged heap memory.</summary>
-    /// <typeparam name="T"></typeparam>
-    public unsafe readonly struct UnmanagedBuffer<T> where T : unmanaged
+    /// <typeparam name="T">Type of element</typeparam>
+    [DebuggerTypeProxy(typeof(UnmanagedBufferDebuggerTypeProxy<>))]
+    [DebuggerDisplay("UnmanagedBuffer<{typeof(T).Name}>[{Length}]")]
+    public unsafe readonly struct UnmanagedBuffer<T> : IDisposable, IEquatable<UnmanagedBuffer<T>> where T : unmanaged
     {
         private readonly T* _pointer;
         private readonly int _length;
+
+        /// <summary>
+        /// Get length of buffer<para/>
+        /// [NOTE] Don't use this property as for-loop condition because of performance. Use <see cref="Span{T}.Length"/> instead.<para/>
+        /// </summary>
+        public readonly int Length => _length;
+
+        /// <summary>Get pointer of buffer head</summary>
+        public readonly T* Ptr => _pointer;
+
+        /// <summary>Get <see cref="Span{T}"/></summary>
+        /// <returns><see cref="Span{T}"/></returns>
+        public readonly Span<T> Span
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => new Span<T>(_pointer, _length);
+        }
 
         /// <summary>
         /// Allocate unmanaged heap memory of specified length.<para/>
@@ -25,11 +46,6 @@ namespace Elffy.Effective
             _pointer = (T*)Marshal.AllocHGlobal(sizeof(T) * length);
             _length = length;
         }
-
-        /// <summary>Get <see cref="Span{T}"/></summary>
-        /// <returns></returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public readonly Span<T> GetSpan() => new Span<T>(_pointer, _length);
 
         /// <summary>Release memory.</summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -47,10 +63,40 @@ namespace Elffy.Effective
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override bool Equals(object? obj) => obj is UnmanagedBuffer<T> buffer && Equals(buffer);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(UnmanagedBuffer<T> other) => EqualityComparer<IntPtr>.Default.Equals((IntPtr)_pointer, (IntPtr)other._pointer) && _length == other._length;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override int GetHashCode() => HashCode.Combine((IntPtr)_pointer, (IntPtr)_length);
+
         private unsafe struct Mock
         {
             public void* Pointer;
             public int Length;
         }
+    }
+
+    internal sealed class UnmanagedBufferDebuggerTypeProxy<T> where T : unmanaged
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly UnmanagedBuffer<T> _entity;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public unsafe T[] Items
+        {
+            get
+            {
+                var items = new T[_entity.Length];
+                for(int i = 0; i < items.Length; i++) {
+                    items[i] = _entity.Ptr[i];
+                }
+                return items;
+            }
+        }
+
+        public UnmanagedBufferDebuggerTypeProxy(UnmanagedBuffer<T> entity) => _entity = entity;
     }
 }
---
+++ src/Elffy/Effective/UnmanagedList.cs
@@ -0,0 +1,128 @@
+﻿#nullable enable
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Elffy.Effective
+{
+    /// <summary>Variable length array implemented by unmanaged memory.</summary>
+    /// <typeparam name="T">Type of element</typeparam>
+    [DebuggerTypeProxy(typeof(UnmanagedListDebuggerTypeProxy<>))]
+    [DebuggerDisplay("UnmanagedList<{typeof(T).Name}>[{Count}]")]
+    public unsafe struct UnmanagedList<T> : IDisposable, IEquatable<UnmanagedList<T>> where T : unmanaged
+    {
+        private UnmanagedBuffer<T> _array;
+        private int _count;
+
+        /// <summary>Get count of elements</summary>
+        public readonly int Count => _count;
+
+        /// <summary>
+        /// Get <see cref="Span{T}"/><para/>
+        /// [NOTE] <see cref="AccessViolationException"/> may happen after inner array changed. Use this property careflly.
+        /// </summary>
+        /// <returns><see cref="Span{T}"/></returns>
+        public Span<T> Span
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => new Span<T>(_array.Ptr, _count);
+        }
+
+        /// <summary>Get pointer of a head element</summary>
+        public readonly T* Ptr => _array.Ptr;
+
+        /// <summary>
+        /// Create variable length array allocated in unmanaged heap memory.<para/>
+        /// [NOTE] You MUST call <see cref="Dispose"/> to release memory !!! Or memory leak occurs.
+        /// </summary>
+        /// <param name="capacity">capacity of inner array</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UnmanagedList(int capacity)
+        {
+            _array = new UnmanagedBuffer<T>(capacity);
+            _count = 0;
+        }
+
+        /// <summary>Add item to tail</summary>
+        /// <param name="item">item</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(T item)
+        {
+            EnsureIndexAccess(_count);
+            _array.Span[_count] = item;
+            _count++;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void AddRange(ReadOnlySpan<T> items)
+        {
+            if(items.Length == 0) { return; }
+            EnsureIndexAccess(_count + items.Length - 1);
+            items.CopyTo(_array.Span.Slice(_count));
+            _count += items.Length;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void RemoveAt(int index)
+        {
+            var span = _array.Span;
+            span.Slice(index + 1, _count - index - 1).CopyTo(span.Slice(index));
+            _array.Ptr[_count] = default;
+            _count--;
+        }
+
+        /// <summary>
+        /// Release memory<para/>
+        /// [NOTE] You MUST call this after using this instance !!! Or memory leak occurs.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose() => _array.Dispose();
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void EnsureIndexAccess(int index)
+        {
+            Debug.Assert(index >= 0);
+            var capacity = _array.Length;
+            if(capacity <= index) {
+                UnmanagedBuffer<T> newArray;
+                checked {
+                    newArray = new UnmanagedBuffer<T>(capacity < 4 ? 4 : (capacity << 1));
+                }
+                _array.Span.CopyTo(newArray.Span);
+                _array.Dispose();
+                _array = newArray;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override bool Equals(object? obj) => obj is UnmanagedList<T> list && Equals(list);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(UnmanagedList<T> other) => _array.Equals(other._array) && _count == other._count;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override int GetHashCode() => HashCode.Combine(_array, _count);
+    }
+
+    internal sealed class UnmanagedListDebuggerTypeProxy<T> where T : unmanaged
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly UnmanagedList<T> _entity;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public unsafe T[] Items
+        {
+            get
+            {
+                var items = new T[_entity.Count];
+                for(int i = 0; i < items.Length; i++) {
+                    items[i] = _entity.Ptr[i];
+                }
+                return items;
+            }
+        }
+
+        public UnmanagedListDebuggerTypeProxy(UnmanagedList<T> entity) => _entity = entity;
+    }
+}
