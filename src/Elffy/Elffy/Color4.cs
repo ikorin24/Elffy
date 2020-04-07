89594a1bc36b169489e4f783678a1f17a599fa49
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-04-08 07:03:01 +0900

    メソッド追加

--- src/Elffy/Elffy/Color4.cs
+++ src/Elffy/Elffy/Color4.cs
@@ -39,6 +39,7 @@ namespace Elffy
         public static implicit operator Color4(TKColor4 color) => Unsafe.As<TKColor4, Color4>(ref color);
         public static explicit operator Color(in Color4 color) => (Color)(TKColor4)color;
         public static implicit operator Color4(in Color color) => (TKColor4)color;
+        public static explicit operator Vector3(Color4 color) => Unsafe.As<Color4, Vector3>(ref color);
 
         private static byte ToByte(float value)
         {
