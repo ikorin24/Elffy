c671144511e96b1788af67916c16f21159d1436d
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-04-11 02:19:33 +0900

    Phong シェーダーを実装

--- src/Elffy/Shading/DefaultShaderSource.cs
+++ src/Elffy/Shading/DefaultShaderSource.cs
@@ -17,110 +17,73 @@ namespace Elffy.Shading
 
         protected override void DefineLocation(VertexDefinition definition)
         {
-            definition.Position("VertexPosition");
-            definition.Normal(1);
-            definition.Color(2);
-            definition.TexCoord(0);
+            definition.Position("vPos");
+            definition.Normal("vNormal");
         }
 
         protected override void SendUniforms(Uniform uniform, Renderable target, in Matrix4 model, in Matrix4 view, in Matrix4 projection)
         {
-            // TODO: 実行テスト用 本来はライティングとマテリアルの情報も引数で渡されるようにする
-
-            uniform.Send("LightPosition", new Vector4(-1f, -1f, 0f, 0f));
-            uniform.Send("LightAmbient",  new Vector3(0.2f, 0.2f, 0.2f));
-            uniform.Send("LightDiffuse",  new Vector3(0.8f, 0.8f, 0.8f));
-            uniform.Send("LightSpecular", new Vector3(1f, 1f, 1f));
-
-            var material = Materials.Plain;
-            uniform.Send("MaterialAmbient", in Unsafe.As<Color4, Vector3>(ref material.Ambient));
-            uniform.Send("MaterialDiffuse", in Unsafe.As<Color4, Vector3>(ref material.Diffuse));
-            uniform.Send("MaterialSpecular", in Unsafe.As<Color4, Vector3>(ref material.Specular));
-            uniform.Send("MaterialShininess", material.Shininess);
-
-            uniform.Send("ModelViewMatrix", view * model);
-            uniform.Send("ProjectionMatrix", projection);
+            var material = new Material(new Color4(0.6f), new Color4(0.35f), new Color4(0f), 0f);
+            uniform.Send("model", model);
+            uniform.Send("view", view);
+            uniform.Send("projection", projection);
+            uniform.Send("eyePos", target.HostScreen.Camera.Position);
+            uniform.Send("lPos", new Vector4(1, 1, 0, 0));
+            uniform.Send("la", new Vector3(0.8f));
+            uniform.Send("ld", new Vector3(0.8f));
+            uniform.Send("ls", new Vector3(1f));
+            uniform.Send("ma", (Vector3)material.Ambient);
+            uniform.Send("md", (Vector3)material.Diffuse);
+            uniform.Send("ms", (Vector3)material.Specular);
+            uniform.Send("shininess", material.Shininess);
         }
 
         private const string VertSource =
 @"#version 440
 
-layout (location = 3) in vec3 VertexPosition;
-layout (location = 1) in vec3 VertexNormal;
-layout (location = 2) in vec4 VertexColor;
-layout (location = 0) in vec2 Texcoord;
-
-uniform vec4 LightPosition;
-uniform vec3 LightAmbient;
-uniform vec3 LightDiffuse;
-uniform vec3 LightSpecular;
-
-uniform vec3 MaterialAmbient;
-uniform vec3 MaterialDiffuse;
-uniform vec3 MaterialSpecular;
-uniform float MaterialShininess;
-
-uniform mat4 ModelViewMatrix;
-uniform mat4 ProjectionMatrix;
-
-out vec3 LightIntensity;
-
-mat3 NormalMatrix;
-mat4 MVP;
-
-
-void getEyeSpace( out vec3 norm, out vec4 position )
-{
-  norm = normalize( NormalMatrix * VertexNormal);
-  position = ModelViewMatrix * vec4(VertexPosition,1.0);
-}
-
-vec3 phongModel( vec4 position, vec3 norm )
-{
-    vec3 s = normalize(vec3(LightPosition - position));
-    vec3 v = normalize(-position.xyz);
-    vec3 r = reflect( -s, norm );
-    vec3 ambient = LightAmbient * MaterialAmbient;
-    float sDotN = max( dot(s,norm), 0.0 );
-    vec3 diffuse = LightDiffuse * MaterialDiffuse * sDotN;
-    vec3 spec = vec3(0.0);
-    if( sDotN > 0.0 )
-        spec = LightSpecular * MaterialSpecular * pow( max( dot(r,v), 0.0 ), MaterialShininess );
-    return ambient + diffuse + spec;
-}
+in vec3 vPos;
+in vec3 vNormal;
+out vec3 posWorld;
+out vec3 normalWorld;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main()
 {
-    MVP = ProjectionMatrix * ModelViewMatrix;
-
-
-    NormalMatrix = transpose(inverse(mat3(ModelViewMatrix)));
-
-
-    vec3 eyeNorm;
-    vec4 eyePosition;
-    /* Get the position and normal in eye space */
-    getEyeSpace(eyeNorm, eyePosition);
-
-    /* Evaluate the lighting equation. */
-    LightIntensity = phongModel( eyePosition, eyeNorm );
-
-    gl_Position = MVP * vec4(VertexPosition,1.0);
+    mat4 modelView = view * model;
+    posWorld = (model * vec4(vPos, 1.0)).xyz;
+    normalWorld = (model * vec4(vNormal, 1.0)).xyz;
+    gl_Position = projection * modelView * vec4(vPos, 1.0);
 }	
 
 ";
 
+        // TODO: shininess の計算方法
         private const string FragSource =
 @"#version 440
 
-out vec4 FragColor;
-
-in vec3 LightIntensity;
+in vec3 posWorld;
+in vec3 normalWorld;
+out vec4 fragColor;
+uniform vec3 eyePos;
+uniform vec4 lPos;
+uniform vec3 la;
+uniform vec3 ld;
+uniform vec3 ls;
+uniform vec3 ma;
+uniform vec3 md;
+uniform vec3 ms;
+uniform float shininess;
 
 void main()
 {
-	FragColor = vec4(LightIntensity, 1.0);
-    //FragColor = vec4(1f, 1f, 1f, 1f);
+    vec3 L = (lPos.w == 0.0) ? normalize(-lPos.xyz) : normalize(posWorld - lPos.xyz / lPos.w);
+    vec3 N = normalize(normalWorld);
+    vec3 R = reflect(L, N);
+    vec3 V = normalize(eyePos - posWorld);
+    vec3 color = (la * ma) + (ld * md * dot(N, L)) + (ls * ms * max(pow(dot(R, V), shininess), 0.0));
+    fragColor = vec4(color, 1.0);
 }
 ";
 
