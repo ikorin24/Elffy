a071f68f8827e5c7e77dcf3f44b0606e26c8f212
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-04-30 11:12:34 +0900

    HashCodeの拡張クラスを追加

---
+++ src/Elffy/Effective/HashCodeExtension.cs
@@ -0,0 +1,173 @@
+﻿#nullable enable
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Elffy.Effective
+{
+    public static class HashCodeEx
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Combine<T1, T2, T3, T4, T5, T6, T7, T8, T9>
+            (T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9)
+        {
+            var hash = new HashCode();
+            hash.Add(v1);
+            hash.Add(v2);
+            hash.Add(v3);
+            hash.Add(v4);
+            hash.Add(v5);
+            hash.Add(v6);
+            hash.Add(v7);
+            hash.Add(v8);
+            hash.Add(v9);
+            return hash.ToHashCode();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Combine<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>
+            (T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10)
+        {
+            var hash = new HashCode();
+            hash.Add(v1);
+            hash.Add(v2);
+            hash.Add(v3);
+            hash.Add(v4);
+            hash.Add(v5);
+            hash.Add(v6);
+            hash.Add(v7);
+            hash.Add(v8);
+            hash.Add(v9);
+            hash.Add(v10);
+            return hash.ToHashCode();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Combine<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>
+            (T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11)
+        {
+            var hash = new HashCode();
+            hash.Add(v1);
+            hash.Add(v2);
+            hash.Add(v3);
+            hash.Add(v4);
+            hash.Add(v5);
+            hash.Add(v6);
+            hash.Add(v7);
+            hash.Add(v8);
+            hash.Add(v9);
+            hash.Add(v10);
+            hash.Add(v11);
+            return hash.ToHashCode();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Combine<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>
+            (T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12)
+        {
+            var hash = new HashCode();
+            hash.Add(v1);
+            hash.Add(v2);
+            hash.Add(v3);
+            hash.Add(v4);
+            hash.Add(v5);
+            hash.Add(v6);
+            hash.Add(v7);
+            hash.Add(v8);
+            hash.Add(v9);
+            hash.Add(v10);
+            hash.Add(v11);
+            hash.Add(v12);
+            return hash.ToHashCode();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Combine<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>
+            (T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13)
+        {
+            var hash = new HashCode();
+            hash.Add(v1);
+            hash.Add(v2);
+            hash.Add(v3);
+            hash.Add(v4);
+            hash.Add(v5);
+            hash.Add(v6);
+            hash.Add(v7);
+            hash.Add(v8);
+            hash.Add(v9);
+            hash.Add(v10);
+            hash.Add(v11);
+            hash.Add(v12);
+            hash.Add(v13);
+            return hash.ToHashCode();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Combine<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>
+            (T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13, T14 v14)
+        {
+            var hash = new HashCode();
+            hash.Add(v1);
+            hash.Add(v2);
+            hash.Add(v3);
+            hash.Add(v4);
+            hash.Add(v5);
+            hash.Add(v6);
+            hash.Add(v7);
+            hash.Add(v8);
+            hash.Add(v9);
+            hash.Add(v10);
+            hash.Add(v11);
+            hash.Add(v12);
+            hash.Add(v13);
+            hash.Add(v14);
+            return hash.ToHashCode();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Combine<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>
+            (T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15)
+        {
+            var hash = new HashCode();
+            hash.Add(v1);
+            hash.Add(v2);
+            hash.Add(v3);
+            hash.Add(v4);
+            hash.Add(v5);
+            hash.Add(v6);
+            hash.Add(v7);
+            hash.Add(v8);
+            hash.Add(v9);
+            hash.Add(v10);
+            hash.Add(v11);
+            hash.Add(v12);
+            hash.Add(v13);
+            hash.Add(v14);
+            hash.Add(v15);
+            return hash.ToHashCode();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Combine<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>
+            (T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16)
+        {
+            var hash = new HashCode();
+            hash.Add(v1);
+            hash.Add(v2);
+            hash.Add(v3);
+            hash.Add(v4);
+            hash.Add(v5);
+            hash.Add(v6);
+            hash.Add(v7);
+            hash.Add(v8);
+            hash.Add(v9);
+            hash.Add(v10);
+            hash.Add(v11);
+            hash.Add(v12);
+            hash.Add(v13);
+            hash.Add(v14);
+            hash.Add(v15);
+            hash.Add(v16);
+            return hash.ToHashCode();
+        }
+    }
+}
