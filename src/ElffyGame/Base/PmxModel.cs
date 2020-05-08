a58120e6189afc614d879790828dfd6886ef01a4
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-05-08 12:44:58 +0900

    テクスチャのロードまでを実装

--- src/ElffyGame/Base/PmxModel.cs
+++ src/ElffyGame/Base/PmxModel.cs
@@ -3,45 +3,76 @@ using System;
 using Elffy;
 using Elffy.Core;
 using Elffy.Effective;
+using Elffy.Imaging;
 using System.Runtime.CompilerServices;
 using OpenTK.Graphics.OpenGL;
 using UnmanageUtility;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Cysharp.Text;
+using PMXParser = MMDTools.PMXParser;
+using PMXObject = MMDTools.PMXObject;
 
 namespace ElffyGame.Base
 {
     public class PmxModel : Renderable
     {
-        private UnmanagedArray<VertexBoneInfo>? _vertexBoneInfo;
-        private UnmanagedArray<Bone>? _bones;
-        private UnmanagedArray<Material>? _materials;
-        private UnmanagedArray<int>? _partVertexCount;
+        private Material[]? _materials;
+        private int[]? _partVertexCount;
 
-        private MMDTools.PMXObject? _pmxObject;
+        private int[]? _textureBufs;
 
-        private bool _resourceReleased;
+        private PMXObject? _pmxObject;
+        private Bitmap[]? _textureBitmaps;
+        private int[]? _textureIndex;
 
-        private readonly object _syncRoot = new object();
-
-        private unsafe PmxModel(MMDTools.PMXObject pmxObject)
+        private unsafe PmxModel(PMXObject pmxObject, Bitmap[] textureBitmaps)
         {
             Debug.Assert(pmxObject != null);
+            Debug.Assert(textureBitmaps != null);
             _pmxObject = pmxObject;
+            _textureBitmaps = textureBitmaps;
             Activated += OnActivated;
             Terminated += OnTerminated;
             Rendered += OnRendered;
         }
 
+        private unsafe void CreateTextures(ReadOnlySpan<Bitmap> images)
+        {
+            var textures = new int[images.Length];
+            for(int i = 0; i < textures.Length; i++) {
+                textures[i] = GL.GenTexture();
+                using(var pixels = images[i].GetPixels(ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb)) {
+                    GL.BindTexture(TextureTarget.Texture2D, textures[i]);
+                    GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.LinearMipmapLinear);
+                    GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+                    GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, pixels.Width, pixels.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, pixels.Ptr);
+                    GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+                    GL.BindTexture(TextureTarget.Texture2D, 0);
+                }
+            }
+            _textureBufs = textures;
+        }
+
+
         private unsafe void OnRendered(Renderable sender, in Matrix4 model, in Matrix4 view, in Matrix4 projection)
         {
             GL.BindVertexArray(VAO);
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, IBO);
-            var partVertexCount = _partVertexCount!.AsSpan();
+            var partVertexCount = _partVertexCount!;
+            var textureIndex = _textureIndex!;
+            var textureBufs = _textureBufs!;
             var pos = 0;
-            foreach(var count in partVertexCount) {
-                GL.DrawElements(BeginMode.Triangles, count, DrawElementsType.UnsignedInt, pos * sizeof(int));
-                pos += count;
+
+
+            //GL.ActiveTexture(TextureUnit.Texture0);       // これが uniform にわたされる sampler になる。この場合 0を渡す。実際に得られるテクスチャは Bind されたテクスチャ
+
+            for(int i = 0; i < partVertexCount.Length; i++) {
+                GL.BindTexture(TextureTarget.Texture2D, textureBufs[textureIndex[i]]);
+                GL.DrawElements(BeginMode.Triangles, partVertexCount[i], DrawElementsType.UnsignedInt, pos * sizeof(int));
+                pos += partVertexCount[i];
             }
         }
 
@@ -59,54 +90,85 @@ namespace ElffyGame.Base
                 Debug.Assert(vertices != null);
                 LoadGraphicBuffer(vertices!.AsSpan(), _pmxObject!.SurfaceList.Span.MarshalCast<MMDTools.Surface, int>());
             }
+
+            var textureBitmaps = _textureBitmaps!;
+            CreateTextures(textureBitmaps);
+            foreach(var t in textureBitmaps) {
+                t.Dispose();
+            }
+
             // not await
             _ = Task.Factory.StartNew(v => ((UnmanagedArray<Vertex>)v)?.Dispose(), vertices);
-            _pmxObject = null;
+            //_pmxObject = null;
         }
 
         private void OnTerminated(FrameObject frameObject)
         {
-            Task.Factory.StartNew(ReleaseResource);
+            if(_textureBufs != null) {
+                GL.DeleteTextures(_textureBufs.Length, _textureBufs);
+                _textureBufs = null;
+            }
+            //Task.Factory.StartNew(ReleaseResource);
         }
 
         private UnmanagedArray<Vertex>? LoadData()
         {
             var pmxObject = _pmxObject!;
-            lock(_syncRoot) {
-                if(_resourceReleased) { return null; }
-                _vertexBoneInfo = pmxObject.VertexList.Span.SelectToUnmanagedArray(ToVertexBoneInfo);
-                _bones = pmxObject.BoneList.Span.SelectToUnmanagedArray(ToBone);
-                _materials = pmxObject.MaterialList.Span.SelectToUnmanagedArray(ToMaterial);
-                _partVertexCount = pmxObject.MaterialList.Span.SelectToUnmanagedArray(m => m.VertexCount);
-            }
-            ReverseTrianglePolygon(pmxObject.SurfaceList.Span);
-            return pmxObject.VertexList.Span.SelectToUnmanagedArray(ToVertex);
-        }
 
-        private void ReleaseResource()
-        {
-            lock(_syncRoot) {
-                _resourceReleased = true;    // Writing boolean is atomic.
-                _vertexBoneInfo?.Dispose();
-                _bones?.Dispose();
-                _materials?.Dispose();
-                _partVertexCount?.Dispose();
-            }
+            _materials = pmxObject.MaterialList.Span.SelectToArray(ToMaterial);
+            _partVertexCount = pmxObject.MaterialList.Span.SelectToArray(m => m.VertexCount);
+            _textureIndex = pmxObject.MaterialList.Span.SelectToArray(m => m.Texture);
+
+            // オリジナルのデータを書き換えているので注意
+            // このメソッドは1回しか通らない前提
+            ReverseTrianglePolygon(pmxObject.SurfaceList.Span);
+
+            return pmxObject.VertexList.Span.SelectToUnmanagedArray(ToVertex);
         }
 
         public static Task<PmxModel> LoadResourceAsync(string name)
         {
             return Task.Factory.StartNew(n =>
             {
-                using var stream = Resources.GetStream((string)n);
-                var pmx = MMDTools.PMXParser.Parse(stream);     // PMXParser.Parse is thread-independed, thread-safe.
-                return new PmxModel(pmx);
+                var name = (string)n;
+                PMXObject pmx;
+                using(var stream = Resources.GetStream(name)) {
+                    pmx = PMXParser.Parse(stream);     // PMXParser.Parse はスレッドセーフ
+                }
+                var textureNames = pmx.TextureList.Span;
+                var dir = Resources.GetDirectoryName(name);
+                var bitmaps = new Bitmap[textureNames.Length];
+                for(int i = 0; i < textureNames.Length; i++) {
+                    var texturePath = PathConcat(dir, textureNames[i].AsSpan());
+                    using(var tStream = Resources.GetStream(texturePath)) {
+                        bitmaps[i] = BitmapHelper.StreamToBitmap(tStream, textureNames[i].FilePathExtension());
+                    }
+                    bitmaps[i].Save(textureNames[i] + ".png");
+                }
+                return new PmxModel(pmx, bitmaps);
             }, name);
         }
 
+        private static string PathConcat(ReadOnlySpan<char> dir, ReadOnlySpan<char> name)
+        {
+            // This method means  $"{dir}/{name}"
+            var length = dir.Length + name.Length + 1;
+
+            var sb = ZString.CreateStringBuilder();
+            var buf = sb.GetSpan(length);
+            dir.CopyTo(buf);
+            buf[dir.Length] = '/';
+            name.CopyTo(buf.Slice(dir.Length + 1));
+            sb.Advance(length);
+            return sb.ToString();
+        }
+
+        /// <summary>三角ポリゴンの表裏を反転させます</summary>
+        /// <param name="surfaceList">頂点インデックス</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static unsafe void ReverseTrianglePolygon(ReadOnlySpan<MMDTools.Surface> surfaceList)
         {
+            // (a, b, c) を (a, c, b) に書き換える
             fixed(MMDTools.Surface* s = surfaceList) {
                 int* p = (int*)s;
                 for(int i = 0; i < surfaceList.Length; i++) {
