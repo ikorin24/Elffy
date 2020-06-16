1b9046a2e3be0bb62cc88908a4b5415bacb8c0a3
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-06-17 03:05:37 +0900

    HiddenWindowを追加

---
+++ src/Elffy/Core/HiddenWindow.cs
@@ -0,0 +1,32 @@
+﻿#nullable enable
+using System;
+using OpenToolkit.Windowing.GraphicsLibraryFramework;
+using GLFWWindow = OpenToolkit.Windowing.GraphicsLibraryFramework.Window;
+
+namespace Elffy.Core
+{
+    internal unsafe class HiddenWindow : IDisposable
+    {
+        private GLFWWindow* _window;
+
+        public HiddenWindow()
+        {
+            GLFW.WindowHint(WindowHintBool.Visible, false);
+            GLFW.WindowHint(WindowHintClientApi.ClientApi, ClientApi.OpenGlApi);
+            GLFW.WindowHint(WindowHintContextApi.ContextCreationApi, ContextApi.NativeContextApi);
+            _window = GLFW.CreateWindow(1, 1, string.Empty, null, null);
+            SwitchContext();
+        }
+
+        public void Dispose()
+        {
+            GLFW.DestroyWindow(_window);
+            _window = null;
+        }
+
+        public void SwitchContext()
+        {
+            GLFW.MakeContextCurrent(_window);
+        }
+    }
+}
