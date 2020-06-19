e89c1ec9f06602a98e585974964b7705a7b0ca4a
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-06-19 18:07:44 +0900

    シェーダーのプリコンパイル用のクラスを追加

---
+++ src/Elffy/OpenGL/GLSLBinary.cs
@@ -0,0 +1,67 @@
+﻿#nullable enable
+using Elffy.Effective;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elffy.OpenGL
+{
+    internal sealed class GLSLBinary
+    {
+        private ValueTypeRentMemory<int> _formats;
+        private ValueTypeRentMemory<byte> _binary;
+
+        private GLSLBinary(ValueTypeRentMemory<int> formats, ValueTypeRentMemory<byte> binary)
+        {
+            _formats = formats;
+            _binary = binary;
+        }
+
+        public static Task<GLSLBinary> LoadAsync(Stream stream, bool closeStream = true)
+        {
+            if(stream is null) { throw new ArgumentNullException(nameof(stream)); }
+
+            return Task.Factory.StartNew(state =>
+            {
+                Debug.Assert(state is Stream);
+                var stream = Unsafe.As<Stream>(state);
+
+                using(var reader = new BinaryReader(stream, Encoding.UTF8, !closeStream)) {
+                    var formatsLen = reader.ReadInt32();
+                    if(formatsLen % sizeof(int) != 0) {
+                        throw new InvalidDataException();
+                    }
+                    var formats = new ValueTypeRentMemory<int>(formatsLen / 4);
+                    reader.Read(formats.Span.MarshalCast<int, byte>());
+                    var binary = new ValueTypeRentMemory<byte>(reader.ReadInt32());
+                    reader.Read(binary.Span);
+                    return new GLSLBinary(formats, binary);
+                }
+            }, stream);
+        }
+
+        public Task DumpAsync(Stream stream, bool closeStream = true)
+        {
+            if(stream is null) { throw new ArgumentNullException(nameof(stream)); }
+            if(!stream.CanWrite) { throw new ArgumentException(); }
+            return Task.Factory.StartNew(state =>
+            {
+                Debug.Assert(state is Stream);
+                var stream = Unsafe.As<Stream>(state);
+                var f = _formats.Span.MarshalCast<int, byte>();
+                var bin = _binary.Span;
+
+                using(var writer = new BinaryWriter(stream, Encoding.UTF8, !closeStream)) {
+                    writer.Write(f.Length);
+                    writer.Write(f);
+                    writer.Write(bin.Length);
+                    writer.Write(bin);
+                }
+            }, stream);
+        }
+    }
+}
---
+++ src/Elffy/OpenGL/ShaderPrecompileHelper.cs
@@ -0,0 +1,69 @@
+﻿#nullable enable
+using OpenToolkit.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Elffy.Effective;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Elffy.Core;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Elffy.OpenGL
+{
+    internal static class ShaderPrecompileHelper
+    {
+        private const int GL_RROGRAM_BINARY_LENGTH = 0x8741;
+        private static int[]? _formats;
+
+        public static unsafe void GetBinary(int program, out ValueTypeRentMemory<byte> binary, out ReadOnlyMemory<int> formats)
+        {
+            formats = GetFormats();
+            binary = default;
+            try {
+                GL.GetProgram(program, (GetProgramParameterName)GL_RROGRAM_BINARY_LENGTH, out int binLen);
+                binary = new ValueTypeRentMemory<byte>(binLen);
+                GL.GetProgramBinary(program, binLen, out int len, out var f, ref binary.Span.GetPinnableReference());
+                Debug.Assert(binLen == len);
+            }
+            catch(Exception ex) {
+                binary.Dispose();
+                binary = default;
+                throw new InvalidOperationException("Some exception thrown. (See inner exception)", ex);
+            }
+        }
+
+        public static unsafe bool TryCreateFromBinary(Span<byte> binary, Span<int> formats, out int shaderProgram)
+        {
+            if(!GetFormats().AsSpan().SequenceEqual(formats)) {
+                shaderProgram = 0;
+                return false;
+            }
+            shaderProgram = GL.CreateProgram();
+            GL.ProgramBinary(shaderProgram,
+                             Unsafe.As<int, BinaryFormat>(ref formats.GetPinnableReference()),
+                             ref binary.GetPinnableReference(),
+                             binary.Length);
+            int linkStatus = 0;
+            GL.GetProgram(shaderProgram, GetProgramParameterName.LinkStatus, &linkStatus);
+            if(linkStatus == Consts.ShaderProgramLinkFailed) {
+                //var log = GL.GetProgramInfoLog(shaderProgram);
+                //throw new InvalidOperationException($"Linking shader is failed.{Environment.NewLine}{log}");
+                shaderProgram = 0;
+                return false;
+            }
+
+            return true;
+        }
+
+        private static int[] GetFormats()
+        {
+            if(_formats is null == false) { return _formats; }
+            GL.GetInteger(GetPName.NumProgramBinaryFormats, out int formatsLen);
+            var format = new int[formatsLen];
+            GL.GetInteger(GetPName.ProgramBinaryFormats, format);
+            return (_formats = format);
+        }
+    }
+}
