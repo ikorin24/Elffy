eb6420ee4ba4188085060747b944bc936b956b8b
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-06-27 08:46:17 +0900

    リソースからのテクスチャ読み込み用の処理を追加

---
+++ src/Elffy/Elffy/ResourceStreamExtension.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+using Elffy.Components;
+using Elffy.Imaging;
+using System;
+using System.IO;
+
+namespace Elffy
+{
+    public static class ResourceStreamExtension
+    {
+        public static Texture ToTexture(this Stream stream, BitmapType bitmapType)
+            => ToTexture(stream, bitmapType, TextureExpansionMode.Bilinear, TextureShrinkMode.NearestNeighbor, TextureMipmapMode.None);
+
+        public static Texture ToTexture(this Stream stream, BitmapType bitmapType, TextureExpansionMode expansionMode, TextureShrinkMode shrinkMode, TextureMipmapMode mipmapMode)
+        {
+            if(stream is null) { throw new ArgumentNullException(nameof(stream)); }
+            using(var bitmap = BitmapHelper.StreamToBitmap(stream, bitmapType)) {
+                var texture = new Texture(expansionMode, shrinkMode, mipmapMode);
+                texture.Load(bitmap);
+                return texture;
+            }
+        }
+    }
+}
