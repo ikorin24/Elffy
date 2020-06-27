6d0ef925766210a666f5c728163e3151e808fd18
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-06-27 10:19:20 +0900

    Cube, Plain のサイズとUV座標を修正

--- src/Elffy/Shape/Plain.cs
+++ src/Elffy/Shape/Plain.cs
@@ -1,6 +1,5 @@
 ﻿#nullable enable
 using Elffy.Core;
-using OpenToolkit;
 using System;
 
 namespace Elffy.Shape
@@ -8,40 +7,27 @@ namespace Elffy.Shape
     /// <summary>正方形の平面の3Dオブジェクトクラス</summary>
     public class Plain : Renderable
     {
+        private const float A = 0.5f;
+
         private static readonly ReadOnlyMemory<Vertex> _vertexArray = new Vertex[4]
         {
-            new Vertex(new Vector3(-1, 1, 0),  new Vector3(0, 0, 1), new Vector2(0, 1)),
-            new Vertex(new Vector3(-1, -1, 0), new Vector3(0, 0, 1), new Vector2(0, 0)),
-            new Vertex(new Vector3(1, -1, 0),  new Vector3(0, 0, 1), new Vector2(1, 0)),
-            new Vertex(new Vector3(1, 1, 0),   new Vector3(0, 0, 1), new Vector2(1, 1)),
-        };
-        private static readonly ReadOnlyMemory<Vertex> _inverseTexCoordYVertexArray = new Vertex[4]
-        {
-            new Vertex(new Vector3(-1, 1, 0),  new Vector3(0, 0, 1), new Vector2(0, 0)),
-            new Vertex(new Vector3(-1, -1, 0), new Vector3(0, 0, 1), new Vector2(0, 1)),
-            new Vertex(new Vector3(1, -1, 0),  new Vector3(0, 0, 1), new Vector2(1, 1)),
-            new Vertex(new Vector3(1, 1, 0),   new Vector3(0, 0, 1), new Vector2(1, 0)),
+            new Vertex(new Vector3(-A, A, 0),  new Vector3(0, 0, 1), new Vector2(0, 0)),
+            new Vertex(new Vector3(-A, -A, 0), new Vector3(0, 0, 1), new Vector2(0, 1)),
+            new Vertex(new Vector3(A, -A, 0),  new Vector3(0, 0, 1), new Vector2(1, 1)),
+            new Vertex(new Vector3(A, A, 0),   new Vector3(0, 0, 1), new Vector2(1, 0)),
         };
 
         private static readonly ReadOnlyMemory<int> _indexArray = new int[6] { 0, 1, 2, 2, 3, 0 };
 
-        private bool _isTexCoordYInversed;
-
         /// <summary>平面の3Dオブジェクトを生成します</summary>
-        public Plain() : this(false) { }
-
-        /// <summary>平面の3Dオブジェクトを生成します</summary>
-        /// <param name="isTexCoordYInversed">テクスチャのY軸方向を反転させる場合 true</param>
-        public Plain(bool isTexCoordYInversed)
+        public Plain()
         {
-            _isTexCoordYInversed = isTexCoordYInversed;
         }
 
         protected override void OnAlive()
         {
             base.OnAlive();
-            var vertexArray = _isTexCoordYInversed ? _inverseTexCoordYVertexArray : _vertexArray;
-            LoadGraphicBuffer(vertexArray.Span, _indexArray.Span);
+            LoadGraphicBuffer(_vertexArray.Span, _indexArray.Span);
         }
     }
 }
--- src/Sandbox/GameStarter.cs
+++ src/Sandbox/GameStarter.cs
@@ -2,10 +2,12 @@
 using Elffy;
 using Elffy.Components;
 using Elffy.Games;
+using Elffy.Imaging;
 using Elffy.Mathematics;
 using Elffy.Shading;
 using Elffy.Shape;
 using Elffy.Threading;
+using Elffy.UI;
 
 namespace Sandbox
 {
@@ -13,51 +15,57 @@ namespace Sandbox
     {
         public static void Start()
         {
-            SingleScreenApp.MainCamera.LookAt(new Vector3(0, 10, 0), new Vector3(0, 10, 50));
+            SingleScreenApp.MainCamera.LookAt(new Vector3(0, 3, 0), new Vector3(0, 4.5f, 20));
 
             new CameraMouse(SingleScreenApp.MainCamera, SingleScreenApp.Mouse, Vector3.Zero).Activate();
 
             var plain = new Plain()
             {
-                Scale = new Vector3(100),
+                Scale = new Vector3(20),
                 Rotation = new Quaternion(Vector3.UnitX, -90f.ToRadian()),
             };
             plain.AddComponent(new Material(new Color4(0.8f), new Color4(0.15f), new Color4(0.2f), 400f));
+            plain.AddComponent(Resources.GetStream("cube.png").ToTexture(BitmapType.Png));
             plain.Activate();
 
             PmxModel.LoadResourceAsync("Alicia/Alicia_solid.pmx").ContinueWithDispatch(model =>
             {
-                model.Scale = new Vector3(0.8f);
+                model.Scale = new Vector3(0.3f);
                 model.AddComponent(new Material(new Color4(0.8f), new Color4(0.25f), new Color4(0.1f), 5f));
                 model.Activate();
             });
 
             PmxModel.LoadResourceAsync("Alicia/Alicia_solid.pmx").ContinueWithDispatch(model =>
             {
-                model.Position = new Vector3(-10, 0, -10);
-                model.Scale = new Vector3(0.8f);
+                model.Position = new Vector3(-5, 0, -5);
+                model.Scale = new Vector3(0.3f);
                 model.Shader = ShaderSource.Normal;
                 model.Activate();
             });
 
             Model3D.LoadResourceAsync("green_frog.fbx").ContinueWithDispatch(model =>
             {
-                model.Scale = new Vector3(0.03f);
-                model.Position = new Vector3(10, 0, 0);
+                model.Scale = new Vector3(0.01f);
+                model.Position = new Vector3(5, 0, 0);
                 model.AddComponent(new Material(new Color4(0f, 0.7f, 0.25f), new Color4(0f, 0.6f, 0.1f), Color4.White, 4));
                 model.Activate();
             });
 
             new Cube()
             {
-                Position = new Vector3(-5, 1, 0),
+                Position = new Vector3(-3, 0.5f, 0),
                 Shader = ShaderSource.Normal,
             }.Activate();
 
-            new Cube()
+            var cube = new Cube()
             {
-                Position = new Vector3(-5, 1, -5),
-            }.Activate();
+                Position = new Vector3(-3, 0.5f, -3),
+            };
+            var cubeTexture = Resources.GetStream("cube.png").ToTexture(BitmapType.Png);
+            cube.AddComponent(cubeTexture);
+            cube.Activate();
+
+            SingleScreenApp.UI.Add(new Button(90, 30));
         }
     }
 }
