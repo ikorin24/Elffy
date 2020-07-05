6d196f42fd9f9487532ba9e6830b00ac2ea12e3d
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-07-06 05:15:14 +0900

    ContinueWithDispatch のオーバーロードを追加

--- src/Elffy.Games/Threading/SingleScreenTaskExtension.cs
+++ src/Elffy.Games/Threading/SingleScreenTaskExtension.cs
@@ -1,10 +1,7 @@
 ﻿#nullable enable
-using Elffy.Games;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Elffy.Threading
@@ -38,7 +35,7 @@ namespace Elffy.Threading
             }, action);
         }
 
-        /// <summary>指定の <see cref="Task{T}"/> の後に Dispatcher スレッドで処理を行います。</summary>
+        /// <summary>指定の <see cref="Task{TResult}"/> の後に Dispatcher スレッドで処理を行います。</summary>
         /// <typeparam name="T">ソースになるタスクの戻り値の型</typeparam>
         /// <param name="task">ソースになるタスク</param>
         /// <param name="action">Dispatcher スレッドで行う処理</param>
@@ -57,7 +54,34 @@ namespace Elffy.Threading
                 var action = Unsafe.As<Action<T>>(state);
 
                 if(t.Exception is null) {
-                    Dispatcher.Invoke(() => action(t.Result));  // TODO: ラムダ式のキャプチャ避けるオーバーロードを Invoke に用意する
+                    Dispatcher.Invoke(() => action(t.Result));  // captuer t, action
+                }
+                else {
+                    throw t.Exception;
+                }
+            }, action);
+        }
+
+        /// <summary>指定の <see cref="Task{TResult}"/> の後に Dispatcher スレッドで処理を行います。</summary>
+        /// <typeparam name="T">ソースになるタスクの戻り値の型</typeparam>
+        /// <param name="task">ソースになるタスク</param>
+        /// <param name="action">Dispatcher スレッドで行う処理</param>
+        public static void ContinueWithDispatch<T>(this Task<T> task, Action<Task, T> action)
+        {
+            if(task is null) { throw new ArgumentNullException(nameof(task)); }
+            if(action is null) { throw new ArgumentNullException(nameof(action)); }
+
+            // 実質的に
+            // action(await task);
+            // と同じだが、元のスレッドが同期コンテキスト持ちのスレッドでなくともスレッド復帰でき、async も不要
+
+            task.ContinueWith((t, state) =>
+            {
+                Debug.Assert(state is Action<Task, T>);
+                var action = Unsafe.As<Action<Task, T>>(state);
+
+                if(t.Exception is null) {
+                    Dispatcher.Invoke(() => action(t, t.Result));   // capture t, action
                 }
                 else {
                     throw t.Exception;
