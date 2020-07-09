1f1f259463dbb86b25feda6387786438854263d4
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-07-09 12:44:15 +0900

    SpanCastUnsafe の FastSpan での実装を改良

---
+++ src/UnitTest/SpanCastUnsafeTest.cs
@@ -0,0 +1,37 @@
+﻿#nullable enable
+using Elffy.Effective.Unsafes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTest
+{
+    public class SpanCastUnsafeTest
+    {
+        [Fact]
+        public void CastRefType()
+        {
+            var objectSpan = new object[100].AsSpan();
+            var objSpanSlice = objectSpan.Slice(0, 10);
+
+            var strSpan = SpanCastUnsafe.CastRefType<object, string>(objSpanSlice);
+            for(int i = 0; i < strSpan.Length; i++) {
+                strSpan[i] = i.ToString();
+            }
+
+            for(int i = 0; i < strSpan.Length; i++) {
+                Assert.True(objSpanSlice[i] is string);
+                Assert.True(strSpan[i] is string);
+                Assert.Same(objSpanSlice[i], strSpan[i]);
+            }
+
+            objSpanSlice.Clear();
+
+            for(int i = 0; i < strSpan.Length; i++) {
+                Assert.Null(objSpanSlice[i]);
+                Assert.Null(strSpan[i]);
+            }
+        }
+    }
+}
