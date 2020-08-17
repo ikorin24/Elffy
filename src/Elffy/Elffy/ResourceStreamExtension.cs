1b6f37c549b4f377346e48fdc91353ac1f1a36de
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-08-18 04:04:57 +0900

    自動で Stream を Dispose するよう修正

--- src/Elffy/Elffy/ResourceStreamExtension.cs
+++ src/Elffy/Elffy/ResourceStreamExtension.cs
@@ -2,6 +2,7 @@
 using Elffy.Components;
 using Elffy.Imaging;
 using System;
+using System.Drawing;
 using System.IO;
 
 namespace Elffy
@@ -11,13 +12,22 @@ namespace Elffy
         public static Texture ToTexture(this Stream stream, BitmapType bitmapType)
             => ToTexture(stream, bitmapType, TextureExpansionMode.Bilinear, TextureShrinkMode.Bilinear, TextureMipmapMode.None);
 
-        public static Texture ToTexture(this Stream stream, BitmapType bitmapType, TextureExpansionMode expansionMode, TextureShrinkMode shrinkMode, TextureMipmapMode mipmapMode)
+        public static Texture ToTexture(this Stream stream, BitmapType bitmapType, TextureExpansionMode expansionMode,
+                                        TextureShrinkMode shrinkMode, TextureMipmapMode mipmapMode,
+                                        bool disposeStream = true)
         {
             if(stream is null) { throw new ArgumentNullException(nameof(stream)); }
-            using(var bitmap = BitmapHelper.StreamToBitmap(stream, bitmapType)) {
-                var texture = new Texture(expansionMode, shrinkMode, mipmapMode);
-                texture.Load(bitmap);
-                return texture;
+            try {
+                using(var bitmap = BitmapHelper.StreamToBitmap(stream, bitmapType)) {
+                    var texture = new Texture(expansionMode, shrinkMode, mipmapMode);
+                    texture.Load(bitmap);
+                    return texture;
+                }
+            }
+            finally {
+                if(disposeStream) {
+                    stream.Dispose();
+                }
             }
         }
     }
