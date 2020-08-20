f9951c1eaf50dfd80edbf35c0ece741583b5c1a7
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-08-20 15:49:20 +0900

    引数を追加

--- src/Elffy/Elffy/ResourceStreamExtension.cs
+++ src/Elffy/Elffy/ResourceStreamExtension.cs
@@ -2,16 +2,35 @@
 using Elffy.Components;
 using Elffy.Imaging;
 using System;
-using System.Drawing;
 using System.IO;
 
 namespace Elffy
 {
     public static class ResourceStreamExtension
     {
-        public static Texture ToTexture(this Stream stream, BitmapType bitmapType)
-            => ToTexture(stream, bitmapType, TextureExpansionMode.Bilinear, TextureShrinkMode.Bilinear, TextureMipmapMode.None);
+        /// <summary>Create <see cref="Texture"/> from <see cref="Stream"/>; (Stream is data as an image file, not pixel raw data.)</summary>
+        /// <remarks>Created <see cref="Texture"/> expands and shrinks linearly, and has no mipmap.</remarks>
+        /// <param name="stream">source stream</param>
+        /// <param name="bitmapType">image file type</param>
+        /// <param name="disposeStream">dispose stream if true. (default is true)</param>
+        /// <returns><see cref="Texture"/> created from <see cref="Stream"/></returns>
+        public static Texture ToTexture(this Stream stream, BitmapType bitmapType, bool disposeStream = true)
+        {
+            return ToTexture(stream, bitmapType, 
+                             TextureExpansionMode.Bilinear,
+                             TextureShrinkMode.Bilinear,
+                             TextureMipmapMode.None,
+                             disposeStream);
+        }
 
+        /// <summary>Create <see cref="Texture"/> from <see cref="Stream"/>; (Stream is data as an image file, not pixel raw data.)</summary>
+        /// <param name="stream">source stream</param>
+        /// <param name="bitmapType">image file type</param>
+        /// <param name="expansionMode">texture expansion mode</param>
+        /// <param name="shrinkMode">textrue shrink mode</param>
+        /// <param name="mipmapMode">texture mipmap mode</param>
+        /// <param name="disposeStream">dispose stream if ture. (default is true)</param>
+        /// <returns><see cref="Texture"/> create from <see cref="Stream"/></returns>
         public static Texture ToTexture(this Stream stream, BitmapType bitmapType, TextureExpansionMode expansionMode,
                                         TextureShrinkMode shrinkMode, TextureMipmapMode mipmapMode,
                                         bool disposeStream = true)
