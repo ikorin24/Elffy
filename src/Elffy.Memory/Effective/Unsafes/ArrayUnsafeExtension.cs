caff450defa6226bfb0a4822c45fde3c6bbce8a4
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-08-27 04:56:28 +0900

    クラスを追加

---
+++ src/Elffy.Memory/Effective/Unsafes/ArrayUnsafeExtension.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+namespace Elffy.Effective.Unsafes
+{
+#if NET5_0
+    public static class ArrayUnsafeExtension
+    {
+        /// <summary>Get array element at specified index without range checking.</summary>
+        /// <remarks>
+        /// This method does not check null reference and does not check index range.
+        /// *** That means UNDEFINED behaviors may occor in this method. ***
+        /// </remarks>
+        /// <typeparam name="T">type of element</typeparam>
+        /// <param name="source">source array</param>
+        /// <param name="index">index of array</param>
+        /// <returns>element in the array</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ref T At<T>(this T[] source, int index)
+        {
+            return ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(source), index);
+        }
+    }
+#endif
+}
