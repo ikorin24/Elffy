4033f4d312b697e7e1b9a56b29f7953aec0373e6
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-09-04 20:13:13 +0900

    不要部を削除

--- src/Elffy/AssemblyServices/AssemblyState.cs
+++ src/Elffy/AssemblyServices/AssemblyState.cs
@@ -8,15 +8,6 @@ namespace Elffy.AssemblyServices
     /// <summary>アセンブリの状態を提供するクラス</summary>
     public static class AssemblyState
     {
-        /// <summary>現在のアセンブリが条件付きコンパイル #define CHECK_ARG でコンパイルされたかどうかを取得します</summary>
-        /// <returns>条件付きコンパイルで CHECK_ARG が定義済みであるかどうか</returns>
-        public const bool IsArgumentChecked =
-#if CHECK_ARG
-            true;
-#else
-            false;
-#endif
-
         /// <summary>現在のアセンブリが条件付きコンパイル #define DEBUG でコンパイルされたかどうかを取得します</summary>
         /// <returns>条件付きコンパイルで DEBUG が定義済みであるかどうか</returns>
         public const bool IsDebug =
