2be902910650e712cd0d33bf62005e275c562f66
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-10-01 04:37:48 +0900

    Add awaitable method

--- src/Elffy.Games/Threading/Tasks/GameAsync.cs
+++ src/Elffy.Games/Threading/Tasks/GameAsync.cs
@@ -1,5 +1,5 @@
 ﻿#nullable enable
-using System.Threading;
+using System;
 using Cysharp.Threading.Tasks;
 using Elffy.Games;
 
@@ -8,26 +8,59 @@ namespace Elffy.Threading.Tasks
     /// <summary>Helper class of short cut to <see cref="Game.AsyncBack"/></summary>
     public static class GameAsync
     {
-        /// <summary>Switch to next update timing by using 'await'.</summary>
+        /// <summary>Wait for the next update timing.</summary>
         /// <returns>awaitable object</returns>
         public static FrameLoopAwaitable ToUpdate()
         {
             return Game.AsyncBack.ToFrameLoopEvent(FrameLoopTiming.Update);
         }
 
+        /// <summary>Wait for the next early update timing.</summary>
+        /// <returns>awaitable object</returns>
         public static FrameLoopAwaitable ToEarlyUpdate()
         {
             return Game.AsyncBack.ToFrameLoopEvent(FrameLoopTiming.EarlyUpdate);
         }
 
+        /// <summary>Wait for the next late update timing.</summary>
+        /// <returns>awaitable object</returns>
         public static FrameLoopAwaitable ToLateUpdate()
         {
             return Game.AsyncBack.ToFrameLoopEvent(FrameLoopTiming.LateUpdate);
         }
 
-        public static FrameLoopAwaitable ToFrameLoopEvent(FrameLoopTiming eventType, CancellationToken cancellationToken = default)
+        /// <summary>Wait for the specified number of frames.</summary>
+        /// <param name="frameCount">number for frames</param>
+        /// <param name="timing">timing of end point</param>
+        /// <returns>awaitable object</returns>
+        public static async UniTask DelayFrame(int frameCount, FrameLoopTiming timing = FrameLoopTiming.Update)
         {
-            return Game.AsyncBack.ToFrameLoopEvent(eventType, cancellationToken);
+            if(frameCount < 0) {
+                throw new ArgumentOutOfRangeException(nameof(frameCount));
+            }
+            for(int i = 0; i < frameCount; i++) {
+                await ToFrameLoopTiming(timing);
+            }
+        }
+
+        /// <summary>Wait for the specified time span.</summary>
+        /// <param name="time">time span for waiting</param>
+        /// <param name="timing">timing of end point</param>
+        /// <returns>awaitable object</returns>
+        public static async UniTask DelayTime(TimeSpan time, FrameLoopTiming timing = FrameLoopTiming.Update)
+        {
+            var start = Game.Screen.Time;
+            while(Game.Screen.Time - start < time) {
+                await ToFrameLoopTiming(timing);
+            }
+        }
+
+        /// <summary>Wait for the specified next frame loop timing.</summary>
+        /// <param name="timing">frame loop timing for waiting</param>
+        /// <returns>awaitable object</returns>
+        public static FrameLoopAwaitable ToFrameLoopTiming(FrameLoopTiming timing)
+        {
+            return Game.AsyncBack.ToFrameLoopEvent(timing, default);
         }
 
         public static SwitchToThreadPoolAwaitable ToThreadPool()
--- src/Elffy/Threading/Tasks/AsyncBackEndPoint.cs
+++ src/Elffy/Threading/Tasks/AsyncBackEndPoint.cs
@@ -5,17 +5,18 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace Elffy.Threading.Tasks
 {
     public sealed class AsyncBackEndPoint
     {
-        private readonly Dictionary<FrameLoopTiming, ConcurrentQueue<Action>> _queues;
+        private readonly ConcurrentDictionary<FrameLoopTiming, ConcurrentQueue<WorkItem>> _queues;
 
         internal AsyncBackEndPoint()
         {
-            _queues = new Dictionary<FrameLoopTiming, ConcurrentQueue<Action>>();
+            _queues = new ConcurrentDictionary<FrameLoopTiming, ConcurrentQueue<WorkItem>>();
         }
 
         public FrameLoopAwaitable ToFrameLoopEvent(FrameLoopTiming timing, CancellationToken cancellationToken = default)
@@ -23,38 +24,82 @@ namespace Elffy.Threading.Tasks
             return new FrameLoopAwaitable(this, timing, cancellationToken);
         }
 
-
-        internal void Post(FrameLoopTiming eventType, Action continuation)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal void Post(FrameLoopTiming timing, Action continuation)
         {
-            if(!_queues.TryGetValue(eventType, out var queue)) {
-                InitQueue(eventType);
-
-                void InitQueue(FrameLoopTiming eventType)
-                {
-                    lock(_queues) {
-                        _queues.TryAdd(eventType, new ConcurrentQueue<Action>());
-                    }
-                }
+            if(continuation is null) { return; }
+            if(!_queues.TryGetValue(timing, out var queue)) {
+                queue = InitQueue(timing);
             }
-            _queues[eventType].Enqueue(continuation);
+            queue.Enqueue(new WorkItem(continuation));
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal void Post(FrameLoopTiming timing, Action<object?> continuation, object? state)
+        {
+            if(continuation is null) { return; }
+            if(!_queues.TryGetValue(timing, out var queue)) {
+                queue = InitQueue(timing);
+            }
+            queue.Enqueue(new WorkItem(continuation, state));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal void DoQueuedEvents(FrameLoopTiming eventType)
         {
             int count;
             if(_queues.TryGetValue(eventType, out var queue) && (count = queue.Count) > 0) {
                 Do(queue, count);
 
-                static void Do(ConcurrentQueue<Action> queue, int count)
+                static void Do(ConcurrentQueue<WorkItem> queue, int count)
                 {
                     for(int i = 0; i < count; i++) {
                         var exists = queue.TryDequeue(out var action);
                         Debug.Assert(exists);
-                        action!();
+                        action.Invoke();
                     }
                 }
             }
             return;
+        }
+
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private ConcurrentQueue<WorkItem> InitQueue(FrameLoopTiming eventType)
+        {
+            var q = new ConcurrentQueue<WorkItem>();
+            _queues.TryAdd(eventType, q);
+            return q;
+        }
+
+        private readonly struct WorkItem
+        {
+            private readonly Action<object?> _action;
+            private readonly object? _state;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public WorkItem(Action<object?> action, object? state)
+            {
+                _action = action;
+                _state = state;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public WorkItem(Action action)
+            {
+                _action = state =>
+                {
+                    Debug.Assert(state is Action);
+                    Unsafe.As<Action>(state).Invoke();
+                };
+                _state = action;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Invoke()
+            {
+                _action.Invoke(_state);
+            }
         }
     }
 }
