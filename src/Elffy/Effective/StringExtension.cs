82ad1a14459fdefbbd4cb1a089d69c1987753dfa
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-10-06 07:25:51 +0900

    Fix something

--- src/Elffy/Effective/StringExtension.cs
+++ src/Elffy/Effective/StringExtension.cs
@@ -7,18 +7,10 @@ namespace Elffy.Effective
     /// <summary><see cref="string"/> 型の拡張メソッド用クラス</summary>
     public static class StringExtension
     {
-        /// <summary>指定の文字列をインターンプールに登録済みの文字列として、その参照を取得します</summary>
-        /// <param name="source">文字列</param>
-        /// <returns>インターンプールの文字列参照</returns>
-        public static string AsInterned(this string source)
-        {
-            return (source is null) ? source! : string.Intern(source);
-        }
-
         /// <summary>指定の文字列が null または空文字列かどうかを取得します。(<see cref="string.IsNullOrEmpty(string)"/> への拡張メソッド)</summary>
         /// <param name="source">文字列</param>
         /// <returns>指定の文字列が null または空文字列かどうか</returns>
-        public static bool IsNullOrEmpty(this string source)
+        public static bool IsNullOrEmpty(this string? source)
         {
             return string.IsNullOrEmpty(source);
         }
@@ -26,7 +18,7 @@ namespace Elffy.Effective
         /// <summary>指定の文字列が null または空白文字列かどうかを取得します。(<see cref="string.IsNullOrWhiteSpace(string)"/> への拡張メソッド)</summary>
         /// <param name="source">文字列</param>
         /// <returns>指定の文字列が null または空白文字列かどうか</returns>
-        public static bool IsNullOrWhiteSpace(this string source)
+        public static bool IsNullOrWhiteSpace(this string? source)
         {
             return string.IsNullOrWhiteSpace(source);
         }
@@ -36,7 +28,9 @@ namespace Elffy.Effective
         /// <returns>拡張子</returns>
         public static ReadOnlySpan<char> FilePathExtension(this string source)
         {
-            if(source is null) { throw new ArgumentNullException(nameof(source)); }
+            if(source is null) {
+                return ReadOnlySpan<char>.Empty;
+            }
             return FilePathExtension(source.AsSpan());
         }
 
