75cc83ea3192f9927ef077ba677a1b99eea397a3
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-10-07 07:18:23 +0900

    Add new method

---
+++ src/Elffy.Memory/Effective/Unsafes/StringUnsafeExtension.cs
@@ -0,0 +1,32 @@
+﻿#nullable enable
+using Elffy.AssemblyServices;
+using System.Runtime.CompilerServices;
+
+namespace Elffy.Effective.Unsafes
+{
+    internal static class StringUnsafeExtension
+    {
+        /// <summary>Get reference to first char of <see cref="string"/></summary>
+        /// <param name="source">source <see cref="string"/></param>
+        /// <returns>reference to first char</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [CriticalDotnetDependency("netcoreapp3.1")]
+        public static ref char GetFirstCharReference(this string source)
+        {
+            return ref Unsafe.As<StringDummy>(source)._firstChar;
+        }
+
+        private sealed class StringDummy
+        {
+#pragma warning disable CS0649  // warning of field not set
+            internal readonly int _stringLength;
+            internal char _firstChar;
+#pragma warning restore CS0649
+
+            private StringDummy()
+            {
+
+            }
+        }
+    }
+}
---
+++ src/UnitTest/StringUnsafeExtensionTest.cs
@@ -0,0 +1,44 @@
+﻿#nullable enable
+using System;
+using Elffy.Core;
+using Elffy.Components;
+using Xunit;
+using Elffy.Effective.Unsafes;
+using System.Runtime.CompilerServices;
+
+namespace UnitTest
+{
+    public class StringUnsafeExtensionTest
+    {
+        [Fact]
+        public void BasicAPI()
+        {
+            // const string
+            Check("This is test");
+            // not const string
+            Check(12345.ToString());
+            // const empty string
+            Check("");
+            // not const empty string
+            var empty = ",".Split(',', StringSplitOptions.None)[0];
+            Assert.True(empty == "");
+            Check(empty);
+        }
+
+        private static unsafe void Check(string str)
+        {
+            ref var firstChar = ref str.GetFirstCharReference();
+
+            if(str.Length > 0) {
+                ref var a = ref Unsafe.AsRef(in str.AsSpan()[0]);
+                var same = Unsafe.AreSame(ref a, ref firstChar);
+                Assert.True(same);
+            }
+            else {
+                // string is null-terminated.
+                // First char of empty string is '\0'
+                Assert.True(firstChar == '\0');
+            }
+        }
+    }
+}
