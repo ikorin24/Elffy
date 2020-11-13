7825920cd66aedd01345a60ac7c33acf668fc09b
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-11-14 01:18:49 +0900

    Add method

--- src/Elffy/Mathematics/MathTool.cs
+++ src/Elffy/Mathematics/MathTool.cs
@@ -102,5 +102,45 @@ namespace Elffy.Mathematics
             }
             return 1uL << (64 - BitOperations.LeadingZeroCount(value - 1));
         }
+
+        /// <summary>Get whether value is power of two or not.</summary>
+        /// <remarks>[NOTE] 0 returns true, and all negative values return false.</remarks>
+        /// <param name="x">value to check</param>
+        /// <returns>value is power of two or not</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsPowerOfTwo(int x)
+        {
+            return (x & (x - 1)) == 0;
+        }
+
+        /// <summary>Get whether value is power of two or not.</summary>
+        /// <remarks>[NOTE] 0 returns true</remarks>
+        /// <param name="x">value to check</param>
+        /// <returns>value is power of two or not</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsPowerOfTwo(uint x)
+        {
+            return (x & (x - 1u)) == 0u;
+        }
+
+        /// <summary>Get whether value is power of two or not.</summary>
+        /// <remarks>[NOTE] 0 returns true, and all negative values return false.</remarks>
+        /// <param name="x">value to check</param>
+        /// <returns>value is power of two or not</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsPowerOfTwo(long x)
+        {
+            return (x & (x - 1L)) == 0L;
+        }
+
+        /// <summary>Get whether value is power of two or not.</summary>
+        /// <remarks>[NOTE] 0 returns true</remarks>
+        /// <param name="x">value to check</param>
+        /// <returns>value is power of two or not</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsPowerOfTwo(ulong x)
+        {
+            return (x & (x - 1uL)) == 0uL;
+        }
     }
 }
