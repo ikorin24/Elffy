876768319aa13cfd8297eca0bb923272af2a161e
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-12-04 13:57:25 +0900

    Add NonCheckingShaderTargetVertexTypeAttribute

---
+++ src/Elffy/Diagnostics/NonCheckingShaderTargetVertexTypeAttribute.cs
@@ -0,0 +1,10 @@
+﻿#nullable enable
+using System;
+
+namespace Elffy.Diagnostics
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public sealed class NonCheckingShaderTargetVertexTypeAttribute : Attribute
+    {
+    }
+}
--- src/Elffy/Diagnostics/ShaderTargetVertexTypeAttribute.cs
+++ src/Elffy/Diagnostics/ShaderTargetVertexTypeAttribute.cs
@@ -1,12 +1,11 @@
 ﻿#nullable enable
-using Cysharp.Text;
 using Elffy.Shading;
 using System;
 using System.Linq;
 
 namespace Elffy.Diagnostics
 {
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
     public sealed class ShaderTargetVertexTypeAttribute : Attribute
     {
         public Type VertexType { get; }
@@ -16,12 +15,17 @@ namespace Elffy.Diagnostics
             VertexType = vertexLikeType ?? throw new ArgumentNullException(nameof(vertexLikeType));
         }
 
-        public static void CheckVertexType(Type shaderSourceType, Type vertexType)
+        internal static void CheckVertexType(Type shaderSourceType, Type vertexType)
         {
             if(shaderSourceType is null) { throw new ArgumentNullException(nameof(shaderSourceType)); }
             if(vertexType is null) { throw new ArgumentNullException(nameof(vertexType)); }
             if(shaderSourceType.IsSubclassOf(typeof(ShaderSource)) == false) {
                 throw new ArgumentException($"{nameof(shaderSourceType)} is not subclass of {typeof(ShaderSource)}");
+            }
+
+            var isNonChecking = GetCustomAttribute(shaderSourceType, typeof(NonCheckingShaderTargetVertexTypeAttribute)) is not null;
+            if(isNonChecking) {
+                return;
             }
 
             var attrs = GetCustomAttributes(shaderSourceType, typeof(ShaderTargetVertexTypeAttribute))
@@ -32,22 +36,9 @@ namespace Elffy.Diagnostics
                     return;
                 }
             }
+
             var availableTypes = string.Join(", ", attrs.Select(a => a.VertexType.FullName));
             throw new ArgumentException($"Vertex type mismatch. Specified vertex type: {vertexType.FullName}. Available vertex type(s): {availableTypes}");
-
-
-            //if(GetCustomAttribute(shaderSourceType, typeof(ShaderTargetVertexTypeAttribute)) is ShaderTargetVertexTypeAttribute attr) {
-
-            //    if(attr.VertexType != vertexType) {
-            //        throw new ArgumentException($"Vertex type mismatch. Shader requires {attr.VertexType.FullName}. Actual vertex is {vertexType.FullName}");
-            //    }
-            //    else {
-            //        return;
-            //    }
-            //}
-            //else {
-            //    throw new ArgumentException($"Shader source does not have {typeof(ShaderTargetVertexTypeAttribute).FullName} attribute.");
-            //}
         }
     }
 }
