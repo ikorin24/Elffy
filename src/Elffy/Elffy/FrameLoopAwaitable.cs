0f730096305790b7eba7b0e4c5b57280450f9f20
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-12-13 05:43:38 +0900

    Add comments

--- src/Elffy/Elffy/FrameLoopAwaitable.cs
+++ src/Elffy/Elffy/FrameLoopAwaitable.cs
@@ -6,6 +6,7 @@ using Cysharp.Threading.Tasks;
 
 namespace Elffy
 {
+    /// <summary>Frame loop awaitable object</summary>
     public readonly struct FrameLoopAwaitable
     {
         private readonly Awaiter _awaiter;
@@ -16,36 +17,58 @@ namespace Elffy
             _awaiter = new Awaiter(endPoint, eventType, cancellationToken);
         }
 
+        /// <summary>Get awaiter of <see cref="FrameLoopAwaitable"/></summary>
+        /// <returns>awaiter</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Awaiter GetAwaiter()
         {
             return _awaiter;
         }
 
+        /// <summary>Creates a continuation that executes when <see cref="FrameLoopAwaitable"/> completes.</summary>
+        /// <param name="continuation">a continuation action</param>
+        /// <returns>awaitable object</returns>
         public async UniTask ContinueWith(Action continuation)
         {
             await this;
             continuation();
         }
 
+        /// <summary>Creates a continuation that executes when <see cref="FrameLoopAwaitable"/> completes.</summary>
+        /// <typeparam name="T">state type</typeparam>
+        /// <param name="state">the argument of the continuation</param>
+        /// <param name="continuation">a continuation action</param>
+        /// <returns>awaitable object</returns>
         public async UniTask ContinueWith<T>(T state, Action<T> continuation)
         {
             await this;
             continuation(state);
         }
 
+        /// <summary>Creates a continuation that executes when <see cref="FrameLoopAwaitable"/> completes.</summary>
+        /// <typeparam name="T">state type</typeparam>
+        /// <param name="continuation">a continuation function</param>
+        /// <returns>awaitable object</returns>
         public async UniTask<T> ContinueWith<T>(Func<T> continuation)
         {
             await this;
             return continuation();
         }
 
+        /// <summary>Creates a continuation that executes when <see cref="FrameLoopAwaitable"/> completes.</summary>
+        /// <typeparam name="T">state type</typeparam>
+        /// <typeparam name="TResult">the argument of the continuation</typeparam>
+        /// <param name="state">the argument of the continuation</param>
+        /// <param name="continuation">a continuation function</param>
+        /// <returns>awaitable object</returns>
         public async UniTask<TResult> ContinueWith<T, TResult>(T state, Func<T, TResult> continuation)
         {
             await this;
             return continuation(state);
         }
 
+        /// <summary>Convert <see cref="FrameLoopAwaitable"/> as <see cref="UniTask"/></summary>
+        /// <returns>awaitable object</returns>
         public async UniTask AsUniTask()
         {
             await this;
