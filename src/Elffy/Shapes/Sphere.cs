84fa58af9f1d75c91eaf847aa28bbb9aae4c7a06
ikorin24 <sunshinexxab@yahoo.co.jp>
2020-12-22 07:26:29 +0900

    Not implemented yet

--- src/Elffy/Shapes/Sphere.cs
+++ src/Elffy/Shapes/Sphere.cs
@@ -1,25 +1,12 @@
 ﻿#nullable enable
 using Elffy.Core;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Elffy.Shapes
 {
+    [Obsolete("Not implemented yet", true)]
     public class Sphere : Renderable
     {
-        private static readonly ReadOnlyMemory<Vertex> _vertexArray;
-        private static readonly ReadOnlyMemory<int> _indexArray;
-
-        static Sphere()
-        {
-            // TODO:
-            _vertexArray = new ReadOnlyMemory<Vertex>();
-            _indexArray = new ReadOnlyMemory<int>();
-        }
-
         public Sphere()
         {
             throw new NotImplementedException();
@@ -27,8 +14,9 @@ namespace Elffy.Shapes
 
         protected override void OnActivated()
         {
+            throw new NotImplementedException();
             base.OnActivated();
-            LoadGraphicBuffer(_vertexArray.Span, _indexArray.Span);
+            //LoadGraphicBuffer(vertexArray.Span, indexArray.Span);
         }
     }
 }
