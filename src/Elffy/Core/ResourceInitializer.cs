0dda3d962aa95a55939bb83c653d3767b8cf4723
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-01-03 02:29:46 +0900

    Fix impl

--- src/Elffy/Core/ResourceInitializer.cs
+++ src/Elffy/Core/ResourceInitializer.cs
@@ -4,92 +4,110 @@ using Elffy.Effective.Unsafes;
 using System;
 using System.Buffers.Binary;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.IO;
 using System.Text;
+using System.Diagnostics.CodeAnalysis;
+using StringLiteral;
 
 namespace Elffy.Core
 {
-    internal static class ResourceInitializer
+    internal static partial class ResourceInitializer
     {
-        private const string FormatVersion = "1.0";
-        private const string MagicWord = "ELFFY_RESOURCE";
-        private static readonly Encoding _utf8 = Encoding.UTF8;
+        [Utf8("1.0")]
+        private static partial ReadOnlySpan<byte> FormatVersion();
+
+        [Utf8("ELFFY_RESOURCE")]
+        private static partial ReadOnlySpan<byte> MagicWord();
 
         public static Dictionary<string, ResourceObject> CreateDictionary(string filePath)
         {
-            using(var fs = AlloclessFileStream.OpenRead(filePath))
-            using(var pooledArray = new PooledArray<byte>(2048)) {
-                var reader = new Reader(fs, pooledArray.InnerArray, _utf8);
+            using var stream = File.OpenRead(filePath);
+            using var pooledArray = new PooledArray<byte>(2048);
 
-                if(reader.ReadString(3) != FormatVersion) {
-                    throw new FormatException();
-                }
-                if(reader.ReadString(MagicWord.Length) != MagicWord) {
-                    throw new FormatException();
-                }
-                var fileCount = reader.ReadInt32();
-                reader.Skip(sizeof(long));  // hash sum
-
-                var dic = new Dictionary<string, ResourceObject>(fileCount);
-                while(!reader.IsEndOfStream) {
-                    //var res = new ResourceObject();
-                    var name = reader.ReadString(reader.ReadInt32());
-                    reader.Skip(sizeof(long));  // time stamp
-                    var len = reader.ReadInt64();
-                    var pos = reader.StreamPosition;
-                    var res = new ResourceObject(len, pos);
-                    dic.Add(name, res);
-                    reader.Skip(res.Length);    // data
-                }
-                return dic;
+            var reader = new Reader(stream, pooledArray.InnerArray);
+            if(!reader.ReadAndEqual(FormatVersion())) {
+                throw new FormatException();
             }
+            if(!reader.ReadAndEqual(MagicWord())) {
+                throw new FormatException();
+            }
+            var fileCount = reader.ReadInt32();
+            reader.Skip(sizeof(long));      // hash sum
+
+            var utf8 = Encoding.UTF8;
+            var dic = new Dictionary<string, ResourceObject>(fileCount);
+            while(!reader.IsEndOfStream) {
+                var name = reader.ReadString(reader.ReadInt32(), utf8);
+                reader.Skip(sizeof(long));  // time stamp
+                var len = reader.ReadInt64();
+                var pos = reader.StreamPosition;
+                var res = new ResourceObject(len, pos);
+                dic.Add(name, res);
+                reader.Skip(res.Length);    // data
+            }
+            return dic;
         }
 
         private readonly ref struct Reader
         {
-            private readonly Stream _stream;
+            // [NOTE]
+            // Use FileStream.Read(Span<byte>) for performance.
+            // The method does not allocate inner buffer.
+
+            private readonly FileStream _stream;
             private readonly byte[] _buf;
-            private readonly Encoding _encoding;
 
             public bool IsEndOfStream => _stream.Position == _stream.Length;
             public long StreamPosition => _stream.Position;
 
-            public Reader(Stream stream, byte[] buf, Encoding encoding)
+            public Reader(FileStream stream, byte[] buf)
             {
                 _stream = stream;
                 _buf = buf;
-                _encoding = encoding;
             }
 
-            public string ReadString(int byteLen)
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public ReadOnlySpan<byte> Read(int byteLen)
             {
-                if(_stream.Read(_buf, 0, byteLen) != byteLen) {
+                var span = _buf.AsSpan(0, byteLen);
+                if(_stream.Read(span) != byteLen) {
                     ThrowEOS();
                 }
-                return _encoding.GetString(_buf, 0, byteLen);
+                return span;
             }
 
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool ReadAndEqual(ReadOnlySpan<byte> data)
+            {
+                return Read(data.Length).SequenceEqual(data);
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public string ReadString(int byteLen ,Encoding encoding)
+            {
+                return encoding.GetString(Read(byteLen));
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public void Skip(long byteLen)
             {
                 _stream.Position += byteLen;
             }
 
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public int ReadInt32()
             {
-                if(_stream.Read(_buf, 0, sizeof(int)) != sizeof(int)) {
-                    ThrowEOS();
-                }
-                return BinaryPrimitives.ReadInt32LittleEndian(_buf);
+                return BinaryPrimitives.ReadInt32LittleEndian(Read(sizeof(int)));
             }
 
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public long ReadInt64()
             {
-                if(_stream.Read(_buf, 0, sizeof(long)) != sizeof(long)) {
-                    ThrowEOS();
-                }
-                return BinaryPrimitives.ReadInt64LittleEndian(_buf);
+                return BinaryPrimitives.ReadInt64LittleEndian(Read(sizeof(long)));
             }
 
+            [DoesNotReturn]
             private static void ThrowEOS() => throw new EndOfStreamException();
         }
     }
