efaae004f92385f6d5b47d021cbeb441d29944bb
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-01-06 17:54:58 +0900

    Fix

--- src/UnitTest/ArrayPooledListCoreTest.cs
+++ src/UnitTest/ArrayPooledListCoreTest.cs
@@ -1,8 +1,5 @@
 ﻿#nullable enable
-using System;
-using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Elffy.Effective;
 using Elffy.Mathematics;
 using Elffy.Core;
 using Xunit;
