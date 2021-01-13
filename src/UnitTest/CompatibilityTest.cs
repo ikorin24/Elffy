6d947dfe7b0a7bd3b8961c99a1f91405a57328ab
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-01-13 20:40:48 +0900

    Add unit test

---
+++ src/UnitTest/CompatibilityTest.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+using System.Runtime.CompilerServices;
+using Elffy;
+using Xunit;
+using SkiaSharp;
+
+namespace UnitTest
+{
+    public class CompatibilityTest
+    {
+        [Fact]
+        public unsafe void SKPoint()
+        {
+            Assert.True(sizeof(SKPoint) == sizeof(float) * 2);
+            Assert.True(sizeof(SKPoint) == sizeof(Vector2));
+            var skPoint = new SKPoint(10f, -40f);
+            ref var vec2 = ref Unsafe.As<SKPoint, Vector2>(ref skPoint);
+
+            Assert.Equal(skPoint.X, vec2.X);
+            Assert.Equal(skPoint.Y, vec2.Y);
+        }
+    }
+}
