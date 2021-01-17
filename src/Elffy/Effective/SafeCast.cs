9f18f9ee22b9bf6b63895b437feb5ab0bcd78557
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-01-17 19:24:53 +0900

    Add comments

--- src/Elffy/Effective/SafeCast.cs
+++ src/Elffy/Effective/SafeCast.cs
@@ -4,8 +4,14 @@ using System.Runtime.CompilerServices;
 
 namespace Elffy
 {
+    /// <summary>Helper class for fast cast</summary>
     internal static class SafeCast
     {
+        /// <summary>Call <see cref="Unsafe.As{T}(object?)"/> with type checking by <see cref="Debug.Assert(bool)"/></summary>
+        /// <remarks><paramref name="value"/> can be null.</remarks>
+        /// <typeparam name="T">type to cast to</typeparam>
+        /// <param name="value">object to cast</param>
+        /// <returns>casted object</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T As<T>(object? value) where T : class
         {
@@ -13,6 +19,11 @@ namespace Elffy
             return Unsafe.As<T>(value!);
         }
 
+        /// <summary>Call <see cref="Unsafe.As{T}(object?)"/> with type checking by <see cref="Debug.Assert(bool)"/></summary>
+        /// <remarks><paramref name="value"/> can not be null.</remarks>
+        /// <typeparam name="T">type to cast to</typeparam>
+        /// <param name="value">object to cast</param>
+        /// <returns>casted object</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T NotNullAs<T>(object value) where T : class
         {
