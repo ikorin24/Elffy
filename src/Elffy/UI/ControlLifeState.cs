3e174496377633d97e4491ccf5e547bfc4aa89e7
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-01-21 10:09:06 +0900

    Add ControlLifeState enum

---
+++ src/Elffy/UI/ControlLifeState.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+
+namespace Elffy.UI
+{
+    /// <summary>Life state of <see cref="Control"/></summary>
+    public enum ControlLifeState : byte
+    {
+        /// <summary>control is new</summary>
+        New,
+        /// <summary>control is in the logical tree. (And in the visual tree.)</summary>
+        InLogicalTree,
+        /// <summary>control is only in the visual tree. (Not in the logical tree.)</summary>
+        InVisualTreeOnly,
+        /// <summary>control is dead, that means it was removed from the logical or visual tree.</summary>
+        Dead,
+    }
+}
