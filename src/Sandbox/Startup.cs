9a65a563a9a483376086145552e3bb27a2b6338f
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-01-23 05:07:42 +0900

    Fix sample

--- src/Sandbox/Startup.cs
+++ src/Sandbox/Startup.cs
@@ -17,91 +17,12 @@ namespace Sandbox
         {
             Definition.Initialize();
 
-            //Hoge().Forget();
-
             await Definition.GenCameraMouse();
 
             await UniTask.WhenAll(
                 Definition.GenAlicia(),
-                UniTask.Create(async () =>
-                {
-                    var dice = Resources.Loader.CreateFbxModel("Dice.fbx");
-                    await dice.ActivateWaitLoaded();
-                    return dice;
-                }));
+                Resources.Loader.CreateFbxModel("Dice.fbx").ActivateWaitLoaded());
             Debug.WriteLine("complete");
-
-            //Resources.Loader
-
-            //new Cube()
-            //{
-            //    Shader = DeferedRenderingShaderSource.Instance,
-            //}.Activate();
-
-            //for(int x = -3; x < 4; x++) {
-            //    for(int y = -3; y < 4; y++) {
-            //        var d = Resources.Loader.CreateFbxModel("Dice.fbx");
-            //        d.Shader = DeferedRenderingShaderSource.Instance;
-            //        d.Position = new(x, y, 0);
-            //        d.Scale = new(0.4f);
-            //        d.Activate();
-            //    }
-            //}
-
-            //await Definition.GenUI();
-
-            //var dice = await Definition.GenDice();
-            //var behavior = await Definition.GenDiceBehavior();
-            //behavior(dice).Forget();
-
-            //Definition.GenKeyBoardInputDump().Forget();
-
-            //await UniTask.WhenAll(
-            //    Definition.GenCameraMouse(),
-            //    Definition.GenPlain(),
-            //    Definition.GenAlicia(),
-            //    Definition.GenFrog(),
-            //    Definition.GenBox2(),
-            //    Definition.GenSky());
-
-        }
-
-        static async UniTaskVoid Hoge()
-        {
-            var tmp = new Cube() { IsVisible = false, };
-            var gBuffer = new GBuffer(Game.Screen);
-            var program = gBuffer.PostProcess.Compile();
-            tmp.Terminated += _ =>
-            {
-                gBuffer.Dispose();
-                program.Dispose();
-            };
-            tmp.Activate();
-            while(true) {
-                await Timing.ToBeforeRendering();
-                gBuffer.BindFrameBuffer();
-                ElffyGL.Clear(ElffyGL.ClearBufferMask.ColorBufferBit | ElffyGL.ClearBufferMask.DepthBufferBit);
-
-                await Timing.ToAfterRendering();
-                FBO.Unbind(FBO.Target.FrameBuffer);
-                program.Render(Game.Screen.ClientSize);
-            }
-
-
-
-            ////var gBuffer = new GBuffer(Game.Screen);
-            ////Game.Screen.PostProcess = gBuffer.PostProcess;
-            //try {
-            //    while(true) {
-            //        await GameAsync.ToBeforeRendering(Game.Screen.RunningToken);
-            //        //gBuffer.BindFrameBuffer();
-            //        await GameAsync.ToAfterRendering(Game.Screen.RunningToken);
-            //    }
-            //}
-            //finally {
-            //    //gBuffer.Dispose();
-            //    System.Diagnostics.Debug.WriteLine("close");
-            //}
         }
     }
 }
