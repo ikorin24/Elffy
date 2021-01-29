fe59e26e45996574253050de5326462fbc3e4d60
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-01-29 16:35:47 +0900

    Add ReadOnlyRawImage

--- src/Elffy.Imaging/RawImage.cs
+++ src/Elffy.Imaging/RawImage.cs
@@ -6,19 +6,28 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Elffy.Imaging
 {
+    [StructLayout(LayoutKind.Explicit)]
     public unsafe readonly struct RawImage : IEquatable<RawImage>
     {
+        // Fields layout must be same as ReadOnlyRawImage.
+
         /// <summary>Get width of image</summary>
+        [FieldOffset(0)]
         public readonly int Width;
         /// <summary>Get height of image</summary>
+        [FieldOffset(4)]
         public readonly int Height;
         /// <summary>Get pixels raw data, which is layouted as (R, G, B, A), each pixel is 4 bytes, each channel is 1 byte.</summary>
+        [FieldOffset(8)]
         public readonly ColorByte* Pixels;
 
-        public static RawImage Empty => default;
-
+        /// <summary>Get or set pixel of specified (x, y)</summary>
+        /// <param name="x">x index (column line)</param>
+        /// <param name="y">y index (row line)</param>
+        /// <returns>pixel</returns>
         public ref ColorByte this[int x, int y]
         {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get
             {
                 if((uint)x >= (uint)Width) {
@@ -31,11 +40,15 @@ namespace Elffy.Imaging
             }
         }
 
+        /// <summary>Get empty <see cref="RawImage"/></summary>
+        public static RawImage Empty => default;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public RawImage(int width, int height, IntPtr pixels) : this(width, height, (ColorByte*)pixels)
         {
-
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public RawImage(int width, int height, ColorByte* pixels)
         {
             if(width < 0) {
@@ -49,14 +62,31 @@ namespace Elffy.Imaging
             Pixels = pixels;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Span<ColorByte> AsSpan()
         {
             return MemoryMarshal.CreateSpan(ref Unsafe.AsRef<ColorByte>(Pixels), Width * Height);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe Span<ColorByte> GetRowLine(int row)
+        {
+            if((uint)row >= (uint)Height) {
+                ThrowOutOfRange(nameof(row));
+            }
+            ref var head = ref Unsafe.AsRef<ColorByte>(Pixels + row * Width);
+            return MemoryMarshal.CreateSpan(ref head, Width);
+        }
+
         public override bool Equals(object? obj) => obj is RawImage image && Equals(image);
 
         public bool Equals(RawImage other) => Width == other.Width && Height == other.Height && Pixels == other.Pixels;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(in RawImage left, in RawImage right) => left.Equals(right);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(in RawImage left, in RawImage right) => !(left == right);
 
         public override int GetHashCode() => HashCode.Combine(Width, Height, (IntPtr)Pixels);
 
--- src/UnitTest/RawImageTest.cs
+++ src/UnitTest/RawImageTest.cs
@@ -1,12 +1,10 @@
 ﻿#nullable enable
+using Elffy;
 using Elffy.Imaging;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Elffy.Effective.Unsafes;
 using Xunit;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 namespace UnitTest
 {
@@ -16,13 +14,26 @@ namespace UnitTest
         public unsafe void RawImageLayout()
         {
             // Layout of Elffy.Imaging.RawImage must be same as OpenTK.Windowing.GraphicsLibraryFramework.Image
+            {
+                Assert.Equal(sizeof(Image), sizeof(RawImage));
+                var rawImage = new RawImage();
+                var image = new Image();
+                Assert.Equal((byte*)&image.Width - (byte*)&image, (byte*)&rawImage.Width - (byte*)&rawImage);
+                Assert.Equal((byte*)&image.Height - (byte*)&image, (byte*)&rawImage.Height - (byte*)&rawImage);
+                Assert.Equal((byte*)&image.Pixels - (byte*)&image, (byte*)&rawImage.Pixels - (byte*)&rawImage);
+            }
 
-            Assert.Equal(sizeof(Image), sizeof(RawImage));
-            var rawImage = new RawImage();
-            var image = new Image();
-            Assert.Equal((byte*)&image.Width - (byte*)&image, (byte*)&rawImage.Width - (byte*)&rawImage);
-            Assert.Equal((byte*)&image.Height - (byte*)&image, (byte*)&rawImage.Height - (byte*)&rawImage);
-            Assert.Equal((byte*)&image.Pixels - (byte*)&image, (byte*)&rawImage.Pixels - (byte*)&rawImage);
+
+            // Layout of RawImage must be same as ReadOnlyRawImage.
+            {
+                Assert.Equal(sizeof(RawImage), sizeof(ReadOnlyRawImage));
+                var pixels = stackalloc ColorByte[60];
+                var rawImage = new RawImage(10, 6, pixels);
+                ref var readOnlyRawImage = ref Unsafe.As<RawImage, ReadOnlyRawImage>(ref rawImage);
+                Assert.True(UnsafeEx.AreSame(in rawImage.Width, in readOnlyRawImage.Width));
+                Assert.True(UnsafeEx.AreSame(in rawImage.Height, in readOnlyRawImage.Height));
+                Assert.True(rawImage.Pixels == UnsafeEx.AsPointer(in readOnlyRawImage.Pixels));
+            }
         }
     }
 }
