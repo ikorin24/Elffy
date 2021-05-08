85d0723ed357c40cfe053ffd82d812f435ddb8f7
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-05-09 03:20:50 +0900

    Fix

---
+++ src/Sandbox/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using Elffy;
+using Elffy.Core;
+
+[assembly: GameLaunchSetting.GenerateMainMethod]
+[assembly: GameLaunchSetting.ScreenSize((int)(1200 * 1.5), (int)(675 * 1.5f))]
+[assembly: GameLaunchSetting.ScreenTitle("Sandbox")]
+[assembly: GameLaunchSetting.ScreenIcon("icon.ico")]
+[assembly: GameLaunchSetting.WindowStyle(WindowStyle.Default)]
+[assembly: GameLaunchSetting.LaunchDevEnv]
+[assembly: GameLaunchSetting.ResourceLoader(typeof(LocalResourceLoader), "Resources.dat")]
+[assembly: GenerateLocalResource("Resources", "Resources.dat")]
