c0cf56ca126db7198bb1e0fcd67ce1b225a5ddf5
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-06-27 05:01:27 +0900

    Add

---
+++ src/Elffy.SimpleKit/Serialization/Fbx/DeformerList.cs
@@ -0,0 +1,40 @@
+﻿#nullable enable
+using FbxTools;
+using Elffy.Effective;
+using System.Runtime.CompilerServices;
+using Elffy.Effective.Unsafes;
+
+namespace Elffy.Serialization.Fbx
+{
+    internal struct DeformerList
+    {
+        private readonly BufferPooledDictionary<long, FbxNode>? _dic;
+
+        public DeformerList(FbxNode objectsNode)
+        {
+            using var buf = new UnsafeRawArray<int>(objectsNode.Children.Count);
+            var count = objectsNode.FindIndexAll(FbxConstStrings.Deformer(), buf.AsSpan());
+            _dic = new BufferPooledDictionary<long, FbxNode>(count);
+            foreach(var index in buf.AsSpan(0, count)) {
+                var deformer = objectsNode.Children[index];
+                var id = deformer.Properties[0].AsInt64();
+                _dic.Add(id, deformer);
+            }
+        }
+
+        public bool TryGetDeformer(long id, out FbxNode deformer)
+        {
+            if(_dic is null) {
+                deformer = default;
+                return false;
+            }
+            return _dic.TryGetValue(id, out deformer);
+        }
+
+        public void Dispose()
+        {
+            _dic?.Dispose();
+            Unsafe.AsRef(_dic) = null;
+        }
+    }
+}
