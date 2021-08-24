be84fe03d8b3842215076664e8bcbf0ee6a45567
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-08-25 05:24:11 +0900

    Add task like type extentions

---
+++ src/Elffy/Threading/TaskExtension.cs
@@ -0,0 +1,93 @@
+﻿#nullable enable
+using Cysharp.Threading.Tasks;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace Elffy.Threading
+{
+    /// <summary>Provides the </summary>
+    public static class TaskExtension
+    {
+        /// <summary>Complete the task synchronously. If it cannot, it will throw an <see cref="InvalidOperationException"/>.</summary>
+        /// <param name="task">the task to complete synchronously</param>
+        public static void SyncGetResult(this UniTask task)
+        {
+            var awaiter = task.GetAwaiter();
+            if(awaiter.IsCompleted) {
+                awaiter.GetResult();
+                return;
+            }
+            ThrowInvalidOperation();
+        }
+
+        /// <summary>Complete the task synchronously. If it cannot, it will throw an <see cref="InvalidOperationException"/>.</summary>
+        /// <param name="task">the task to complete synchronously</param>
+        public static void SyncGetResult(this ValueTask task)
+        {
+            var awaiter = task.GetAwaiter();
+            if(awaiter.IsCompleted) {
+                awaiter.GetResult();
+                return;
+            }
+            ThrowInvalidOperation();
+        }
+
+        /// <summary>Complete the task synchronously. If it cannot, it will throw an <see cref="InvalidOperationException"/>.</summary>
+        /// <param name="task">the task to complete synchronously</param>
+        public static void SyncGetResult(this Task task)
+        {
+            var awaiter = task.GetAwaiter();
+            if(awaiter.IsCompleted) {
+                awaiter.GetResult();
+                return;
+            }
+            ThrowInvalidOperation();
+        }
+
+        /// <summary>Complete the task synchronously. If it cannot, it will throw an <see cref="InvalidOperationException"/>.</summary>
+        /// <typeparam name="T">type of the result</typeparam>
+        /// <param name="task">the task to complete</param>
+        /// <returns>result of the task</returns>
+        public static T SyncGetResult<T>(this UniTask<T> task)
+        {
+            var awaiter = task.GetAwaiter();
+            if(awaiter.IsCompleted) {
+                return awaiter.GetResult();
+            }
+            ThrowInvalidOperation();
+            return default;
+        }
+
+        /// <summary>Complete the task synchronously. If it cannot, it will throw an <see cref="InvalidOperationException"/>.</summary>
+        /// <typeparam name="T">type of the result</typeparam>
+        /// <param name="task">the task to complete</param>
+        /// <returns>result of the task</returns>
+        public static T SyncGetResult<T>(this ValueTask<T> task)
+        {
+            var awaiter = task.GetAwaiter();
+            if(awaiter.IsCompleted) {
+                return awaiter.GetResult();
+            }
+            ThrowInvalidOperation();
+            return default;
+        }
+
+        /// <summary>Complete the task synchronously. If it cannot, it will throw an <see cref="InvalidOperationException"/>.</summary>
+        /// <typeparam name="T">type of the result</typeparam>
+        /// <param name="task">the task to complete</param>
+        /// <returns>result of the task</returns>
+        public static T SyncGetResult<T>(this Task<T> task)
+        {
+            var awaiter = task.GetAwaiter();
+            if(awaiter.IsCompleted) {
+                return awaiter.GetResult();
+            }
+            ThrowInvalidOperation();
+            return default;
+        }
+
+        [DoesNotReturn]
+        private static void ThrowInvalidOperation() => throw new InvalidOperationException("Could not complete the task synchronously.");
+    }
+}
---
+++ src/UnitTest/TaskExtensionTest.cs
@@ -0,0 +1,56 @@
+﻿#nullable enable
+using Cysharp.Threading.Tasks;
+using Elffy.Threading;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTest
+{
+    public class TaskExtensionTest
+    {
+        [Fact]
+        public void SyncGetResult()
+        {
+            // It must complete the task synchronously.
+            {
+                UniTask.CompletedTask.SyncGetResult();
+                Assert.Equal(0, new UniTask<int>(0).SyncGetResult());
+                Assert.Equal("aaa", new UniTask<string>("aaa").SyncGetResult());
+                Assert.Equal(AsyncUnit.Default, new UniTask<AsyncUnit>(AsyncUnit.Default).SyncGetResult());
+            }
+            {
+                ValueTask.CompletedTask.SyncGetResult();
+                Assert.Equal(0, new ValueTask<int>(0).SyncGetResult());
+                Assert.Equal("aaa", new ValueTask<string>("aaa").SyncGetResult());
+                Assert.Equal(AsyncUnit.Default, new ValueTask<AsyncUnit>(AsyncUnit.Default).SyncGetResult());
+            }
+            {
+                Task.CompletedTask.SyncGetResult();
+                Assert.Equal(0, new ValueTask<int>(0).AsTask().SyncGetResult());
+                Assert.Equal("aaa", new ValueTask<string>("aaa").AsTask().SyncGetResult());
+                Assert.Equal(AsyncUnit.Default, new ValueTask<AsyncUnit>(AsyncUnit.Default).AsTask().SyncGetResult());
+            }
+
+            // It must throw an exception when we try to get result.
+            {
+                Assert.Throws<InvalidOperationException>(() => UniTask.Never(default).SyncGetResult());
+                Assert.Throws<InvalidOperationException>(() => UniTask.Never<int>(default).SyncGetResult());
+                Assert.Throws<InvalidOperationException>(() => UniTask.Never<string>(default).SyncGetResult());
+                Assert.Throws<InvalidOperationException>(() => UniTask.Never<AsyncUnit>(default).SyncGetResult());
+            }
+            {
+                Assert.Throws<InvalidOperationException>(() => UniTask.Never(default).AsValueTask().SyncGetResult());
+                Assert.Throws<InvalidOperationException>(() => UniTask.Never<int>(default).AsValueTask().SyncGetResult());
+                Assert.Throws<InvalidOperationException>(() => UniTask.Never<string>(default).AsValueTask().SyncGetResult());
+                Assert.Throws<InvalidOperationException>(() => UniTask.Never<AsyncUnit>(default).AsValueTask().SyncGetResult());
+            }
+            {
+                Assert.Throws<InvalidOperationException>(() => UniTask.Never(default).AsTask().SyncGetResult());
+                Assert.Throws<InvalidOperationException>(() => UniTask.Never<int>(default).AsTask().SyncGetResult());
+                Assert.Throws<InvalidOperationException>(() => UniTask.Never<string>(default).AsTask().SyncGetResult());
+                Assert.Throws<InvalidOperationException>(() => UniTask.Never<AsyncUnit>(default).AsTask().SyncGetResult());
+            }
+        }
+    }
+}
