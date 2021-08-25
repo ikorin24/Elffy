f9b1bcbcbf17b458ddfd0a875e88b58df7cefbfd
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-08-26 06:50:22 +0900

    Fix

--- src/Elffy.SimpleKit/RigVertex.cs
+++ src/Elffy.SimpleKit/RigVertex.cs
@@ -4,7 +4,7 @@ using Elffy;
 using Elffy.Core;
 
 // Auto generate vertex type by a source generator
-[assembly: GenerateCustomVertex(nameof(Elffy) + ".RigVertex",
+[assembly: GenerateCustomVertex(nameof(Elffy) + "." + nameof(RigVertex),
     "Position", typeof(Vector3),  VertexSpecialField.Position, 0,  VertexFieldMarshalType.Float, 3,
     "Normal",   typeof(Vector3),  VertexSpecialField.Normal,   12, VertexFieldMarshalType.Float, 3,
     "UV",       typeof(Vector2),  VertexSpecialField.UV,       24, VertexFieldMarshalType.Float, 2,
