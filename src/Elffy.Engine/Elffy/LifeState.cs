6529988da152c9dba50cbe83c2f50a7a678cd66c
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-10-07 02:20:49 +0900

    Use generator

--- src/Elffy.Engine/Elffy/LifeState.cs
+++ src/Elffy.Engine/Elffy/LifeState.cs
@@ -5,58 +5,31 @@ using System.Runtime.CompilerServices;
 namespace Elffy
 {
     /// <summary>Life states of <see cref="FrameObject"/></summary>
-    public enum LifeState : byte
-    {
-        /// <summary>Initial state of <see cref="FrameObject"/>. Not managed by the engine.</summary>
-        New = 0,
-
-        Activating = 1,
-        /// <summary>State that <see cref="FrameObject"/> is in the activation queue. (It is not running yet, it gets alive in the next frame.)</summary>
-        Activated = 2,
-        /// <summary>State that <see cref="FrameObject"/> is running.</summary>
-        Alive = 3,
-        /// <summary>State that <see cref="FrameObject"/> is in the termination queue. (It is still running, it gets dead in the next frame.)</summary>
-        Terminated = 4,
-        /// <summary>State that <see cref="FrameObject"/> is dead. Not managed by the engine.</summary>
-        Dead = 5,
-    }
-
-    public static class LifeStateExtension
+    [GenerateEnumLikeStruct(typeof(byte))]
+    [EnumLikeValue("New", 0)]
+    [EnumLikeValue("Activating", 1)]
+    [EnumLikeValue("Activated", 2)]
+    [EnumLikeValue("Alive", 3)]
+    [EnumLikeValue("Terminated", 4)]
+    [EnumLikeValue("Dead", 5)]
+    public partial struct LifeState
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool Is(this LifeState source, LifeState state)
-        {
-            return source == state;
-        }
+        public bool Is(LifeState state) => _value == state._value;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsBefore(this LifeState source, LifeState state)
-        {
-            return (byte)source < (byte)state;
-        }
+        public bool IsBefore(LifeState state) => _value < state._value;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsSameOrBefore(this LifeState source, LifeState state)
-        {
-            return (byte)source <= (byte)state;
-        }
+        public bool IsSameOrBefore(LifeState state) => _value <= state._value;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsAfter(this LifeState source, LifeState state)
-        {
-            return (byte)source > (byte)state;
-        }
+        public bool IsAfter(LifeState state) => _value > state._value;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsSameOrAfter(this LifeState source, LifeState state)
-        {
-            return (byte)source >= (byte)state;
-        }
+        public bool IsSameOrAfter(LifeState state) => _value >= state._value;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsRunning(this LifeState source)
-        {
-            return source == LifeState.Alive || source == LifeState.Terminated;
-        }
+        public bool IsRunning() => this == Alive || this == Terminated;
     }
 }
