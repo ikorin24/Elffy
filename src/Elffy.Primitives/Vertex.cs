ad0c36d5a3a28f877e96e696ed9b34116c5afc96
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-10-15 03:00:19 +0900

    Remove files and move code

--- src/Elffy.Primitives/Vertex.cs
+++ src/Elffy.Primitives/Vertex.cs
@@ -13,4 +13,30 @@ namespace Elffy
     public partial struct Vertex
     {
     }
+
+    /// <summary>Slim vertex struct, which has position and uv.</summary>
+    /// <remarks>If you need "Normal", use <see cref="Vertex"/> instead.</remarks>
+    [DebuggerDisplay("{Position}")]
+    [GenerateCustomVertex(
+        "Position", typeof(Vector3), VertexSpecialField.Position, 0, VertexFieldMarshalType.Float, 3,
+        "UV", typeof(Vector2), VertexSpecialField.UV, 12, VertexFieldMarshalType.Float, 2
+    )]
+    public partial struct VertexSlim
+    {
+    }
+
+
+    /// <summary>Skinned vertex struct, which has position, normal, uv, bone, weight, texture-index</summary>
+    [DebuggerDisplay("{Position}")]
+    [GenerateCustomVertex(
+        "Position", typeof(Vector3), VertexSpecialField.Position, 0, VertexFieldMarshalType.Float, 3,
+        "Normal", typeof(Vector3), VertexSpecialField.Normal, 12, VertexFieldMarshalType.Float, 3,
+        "UV", typeof(Vector2), VertexSpecialField.UV, 24, VertexFieldMarshalType.Float, 2,
+        "Bone", typeof(Vector4i), VertexSpecialField.Bone, 32, VertexFieldMarshalType.Int32, 4,
+        "Weight", typeof(Vector4), VertexSpecialField.Weight, 48, VertexFieldMarshalType.Float, 4,
+        "TextureIndex", typeof(int), VertexSpecialField.TextureIndex, 64, VertexFieldMarshalType.Int32, 1
+    )]
+    public partial struct SkinnedVertex
+    {
+    }
 }
