dde13fb925b5a4abd313f6b28c64335a923d2f84
ikorin24 <sunshinexxab@yahoo.co.jp>
2021-12-12 04:21:40 +0900

    Fix

--- src/Elffy.SimpleKit/Serialization/PmxModelBuilder.cs
+++ src/Elffy.SimpleKit/Serialization/PmxModelBuilder.cs
@@ -11,6 +11,8 @@ using Elffy.Shading.Forward;
 using Cysharp.Threading.Tasks;
 using MMDTools.Unmanaged;
 
+using System.Linq;
+
 namespace Elffy.Serialization
 {
     public static class PmxModelBuilder
@@ -49,23 +51,19 @@ namespace Elffy.Serialization
             // ------------------------------
             //      ↓ thread pool
             Debug.Assert(Engine.IsThreadMain == false);
-            using var modelData = await UniTask.WhenAll(
-                    BuildVertices(pmx),
-                    BuildBones(pmx),
-                    LoadTextureImages(pmx, obj.File))
-                .ContinueWith(d => new ModelData(d.Item1, d.Item2, d.Item3));
-
-            // create skeleton
             model.TryGetHostScreen(out var screen);
             Debug.Assert(screen is not null);
 
             // Don't make the followings parallel like UniTask.WhenAll.
             // ModelData will be disposed when an exception is throw, but another parallel code could not know that.
-            await BuildAndAttachArrayTexture(model, modelData.Images.AsSpan(), new Vector2i(1024, 1024), screen.TimingPoints.Update, obj.CancellationToken);
+            using var vertices = GetVertices(pmx);
+            using var bones = GetBones(pmx);
+            await BuildTexture(pmx, obj.File, model, vertices);
             await UniTask.SwitchToThreadPool();
+
             var skeleton = new HumanoidSkeleton();
             model.AddComponent(skeleton);
-            await skeleton.LoadAsync(modelData.Bones.AsSpanLike(), screen.TimingPoints, obj.CancellationToken);
+            await skeleton.LoadAsync(bones.AsSpanLike(), screen.TimingPoints, obj.CancellationToken);
 
             //      ↑ thread pool
             // ------------------------------
@@ -78,160 +76,138 @@ namespace Elffy.Serialization
             model.Shader = PmxModelShader.Instance;
 
             // load vertices and indices
-            load.Invoke(modelData.Vertices.AsSpan().AsReadOnly(), pmx.SurfaceList.AsSpan().MarshalCast<Surface, int>());
+            load.Invoke(vertices.AsSpan().AsReadOnly(), pmx.SurfaceList.AsSpan().MarshalCast<Surface, int>());
 
             //      ↑ main thread
             // ------------------------------
         }
 
-        private static async UniTask<UnsafeRawArray<SkinnedVertex>> BuildVertices(PMXObject pmx)
+        private static UnsafeRawArray<SkinnedVertex> GetVertices(PMXObject pmx)
         {
-            await UniTask.SwitchToThreadPool();
-            return await OnThreadPool(pmx);
-
-            static UniTask<UnsafeRawArray<SkinnedVertex>> OnThreadPool(PMXObject pmx)
-            {
-                // build vertices
-                var pmxVertices = pmx.VertexList.AsSpan();
-                var materials = pmx.MaterialList.AsSpan();
-                var vertices = new UnsafeRawArray<SkinnedVertex>(pmxVertices.Length, false);
-                try {
-
-                    for(int i = 0; i < pmxVertices.Length; i++) {
-                        vertices[i] = pmxVertices[i].ToRigVertex();
-                    }
-
-                    {
-                        var indices = pmx.SurfaceList.AsSpan().MarshalCast<Surface, int>();
-                        int i = 0;
-                        foreach(var mat in materials) {
-                            for(int j = 0; j < mat.VertexCount; j++) {
-                                vertices[indices[i++]].TextureIndex = mat.Texture;
-                            }
-                        }
-                    }
-                }
-                catch {
-                    vertices.Dispose();
-                    throw;
-                }
-                return UniTask.FromResult(vertices);
-            }
-        }
-
-        private static async UniTask<UnsafeRawArray<Components.Bone>> BuildBones(PMXObject pmx)
-        {
-            await UniTask.SwitchToThreadPool();
-            return await OnThreadPool(pmx);
-
-            static UniTask<UnsafeRawArray<Components.Bone>> OnThreadPool(PMXObject pmx)
-            {
-                var pmxBones = pmx.BoneList.AsSpan();
-                var bones = new UnsafeRawArray<Components.Bone>(pmxBones.Length, false);
-                try {
-                    for(int i = 0; i < pmxBones.Length; i++) {
-                        var parentBone = pmxBones[i].ParentBone >= 0 ? pmxBones[i].ParentBone : (int?)null;
-                        Vector3 boneVec;
-                        var c = pmxBones[i].ConnectedBone;
-                        if(c >= 0) {
-                            boneVec = pmxBones[c].Position.ToVector3() - pmxBones[i].Position.ToVector3();
-                        }
-                        else {
-                            boneVec = pmxBones[i].PositionOffset.ToVector3();
-                        }
-
-                        bones[i] = new Components.Bone();
-
-                    }
-                }
-                catch {
-                    bones.Dispose();
-                    throw;
-                }
-                return UniTask.FromResult(bones);
-            }
-        }
-
-        private static async UniTask<RefTypeRentMemory<IImageSource?>> LoadTextureImages(PMXObject pmx, ResourceFile pmxFile)
-        {
-            await UniTask.SwitchToThreadPool();
-            return await OnThreadPool(pmx, pmxFile);
-
-            static UniTask<RefTypeRentMemory<IImageSource?>> OnThreadPool(PMXObject pmx, ResourceFile pmxFile)
-            {
-                var dir = ResourcePath.GetDirectoryName(pmxFile.Name);
-                var textureNames = pmx.TextureList.AsSpan();
-                var resourceLoader = pmxFile.ResourceLoader;
-                var images = new RefTypeRentMemory<IImageSource?>(textureNames.Length);
-
-                //var materials = pmx.MaterialList.AsSpan();
-                //using var matTexMem = new ValueTypeRentMemory<int>(materials.Length, true);
-                //var matTex = matTexMem.AsSpan();
-                //for(int i = 0; i < materials.Length; i++) {
-                //    matTex[i] = materials[i].Texture;
-                //}
-
-                try {
-                    for(int i = 0; i < textureNames.Length; i++) {
-                        //if(matTex.Contains(i) == false) { continue; }
-
-                        using var _ = GetTexturePath(dir, textureNames[i], out var texturePath, out var ext);
-                        var path = texturePath.ToString();
-
-                        // Some pmx have the texture paths that don't exist. (Nobody references them.)
-                        // So skip them.
-                        if(resourceLoader.TryGetStream(path, out var stream)) {
-                            try {
-                                images[i] = Image.LoadToImageSource(stream, Image.GetTypeFromExt(ext));
-                            }
-                            finally {
-                                stream.Dispose();
-                            }
-                        }
-                        else {
-                            Debug.WriteLine($"not exist: {path}");
-                        }
-                    }
-                    return UniTask.FromResult(images);
-                }
-                catch {
-                    foreach(var image in images.AsSpan()) {
-                        image?.Dispose();
-                    }
-                    throw;
+            // build vertices
+            var pmxVertices = pmx.VertexList.AsSpan();
+            var vertices = new UnsafeRawArray<SkinnedVertex>(pmxVertices.Length, false);
+            try {
+                for(int i = 0; i < pmxVertices.Length; i++) {
+                    vertices[i] = pmxVertices[i].ToRigVertex();
                 }
             }
+            catch {
+                vertices.Dispose();
+                throw;
+            }
+            return vertices;
         }
 
-        private static UniTask BuildAndAttachArrayTexture(Model3D model, ReadOnlySpan<IImageSource?> images, Vector2i size, FrameTimingPoint timingPoint, CancellationToken ct)
+        private static UnsafeRawArray<Components.Bone> GetBones(PMXObject pmx)
+        {
+            var pmxBones = pmx.BoneList.AsSpan();
+            var bones = new UnsafeRawArray<Components.Bone>(pmxBones.Length, false);
+            try {
+                for(int i = 0; i < pmxBones.Length; i++) {
+                    var parentBone = pmxBones[i].ParentBone >= 0 ? pmxBones[i].ParentBone : (int?)null;
+                    Vector3 boneVec;
+                    var c = pmxBones[i].ConnectedBone;
+                    if(c >= 0) {
+                        boneVec = pmxBones[c].Position.ToVector3() - pmxBones[i].Position.ToVector3();
+                    }
+                    else {
+                        boneVec = pmxBones[i].PositionOffset.ToVector3();
+                    }
+
+                    bones[i] = new Components.Bone();
+
+                }
+            }
+            catch {
+                bones.Dispose();
+                throw;
+            }
+            return bones;
+        }
+
+        private static UniTask BuildTexture(PMXObject pmx, ResourceFile pmxFile, Model3D model, UnsafeRawArray<SkinnedVertex> vertices)
         {
             var arrayTexture = new ArrayTexture(TextureConfig.BilinearRepeat);
             model.AddComponent(arrayTexture);
-            if(images.IsEmpty) {
-                return UniTask.CompletedTask;
+
+            var dir = ResourcePath.GetDirectoryName(pmxFile.Name);
+            var textureNames = pmx.TextureList.AsSpan();
+            var resourceLoader = pmxFile.ResourceLoader;
+
+            var materials = pmx.MaterialList.AsSpan();
+            using var matTexMem = new ValueTypeRentMemory<int>(materials.Length, true);
+            var matTex = matTexMem.AsSpan();
+            for(int i = 0; i < materials.Length; i++) {
+                matTex[i] = materials[i].Texture;
             }
-            var count = 0;
-            var buffer = new ValueTypeRentMemory<ColorByte>(size.X * size.Y * images.Length, false);
-            try {
-                foreach(var image in images) {
-                    ct.ThrowIfCancellationRequested();
-                    if(image == null) { continue; }
-                    var dest = buffer.AsSpan(count * size.X * size.Y);
-                    if(image.Width != size.X || image.Height != size.Y) {
-                        using var resized = Image.Resized(image, size);
-                        resized.GetPixels().CopyTo(dest);
+            matTex.Sort();
+            var texCount = 0;
+            if(matTex.Length > 0) {
+                texCount = 1;
+                for(int i = 1; i < matTex.Length; i++) {
+                    if(matTex[i - 1] != matTex[i]) {
+                        texCount++;
                     }
-                    else {
-                        image.GetPixels().CopyTo(dest);
-                    }
-                    count++;
                 }
+            }
+
+            using var textureUsed = new ValueTypeRentMemory<bool>(textureNames.Length, true);
+
+            var size = new Vector2i(1024, 1024);    // TODO:
+            var count = 0;
+            var buffer = new ValueTypeRentMemory<ColorByte>(size.X * size.Y * texCount, false);
+            try {
+                for(int i = 0; i < textureNames.Length; i++) {
+                    if(matTex.Contains(i) == false) { continue; }
+
+                    using var _ = GetTexturePath(dir, textureNames[i], out var texturePath, out var ext);
+                    var path = texturePath.ToString();
+
+                    // Some pmx have the texture paths that don't exist. (Nobody references them.)
+                    // So skip them.
+                    if(resourceLoader.TryGetStream(path, out var stream) == false) { continue; }
+                    textureUsed[i] = true;
+                    var dest = buffer.AsSpan(count * size.X * size.Y);
+                    try {
+                        using var image = Image.LoadToImageSource(stream, Image.GetTypeFromExt(ext));
+                        if(image.Width != size.X || image.Height != size.Y) {
+                            using var resized = Image.Resized(image, size);
+                            resized.GetPixels().CopyTo(dest);
+                        }
+                        else {
+                            image.GetPixels().CopyTo(dest);
+                        }
+                        count++;
+                    }
+                    finally {
+                        stream.Dispose();
+                    }
+                }
+
+                // Calculate texture index of each vertex
+                {
+                    var indices = pmx.SurfaceList.AsSpan().MarshalCast<Surface, int>();
+                    int i = 0;
+                    foreach(var mat in materials) {
+                        for(int j = 0; j < mat.VertexCount; j++) {
+                            var unusedCount = 0;
+                            foreach(var isUsed in textureUsed.AsSpan(0, mat.Texture)) {
+                                if(isUsed == false) {
+                                    unusedCount++;
+                                }
+                            }
+                            vertices[indices[i++]].TextureIndex = mat.Texture - unusedCount;
+                        }
+                    }
+                }
+
             }
             catch {
                 buffer.Dispose();
                 throw;
             }
-            return BackToMainThread(arrayTexture, new Vector3i(size.X, size.Y, count), buffer, timingPoint, ct);
+            return BackToMainThread(arrayTexture, new Vector3i(size.X, size.Y, count), buffer, model.Screen!.TimingPoints.Update, CancellationToken.None);  // TODO: CancellationToken
 
             static async UniTask BackToMainThread(ArrayTexture arrayTexture, Vector3i size, ValueTypeRentMemory<ColorByte> buffer, FrameTimingPoint timingPoint, CancellationToken ct)
             {
@@ -244,6 +220,7 @@ namespace Elffy.Serialization
                 }
             }
         }
+
 
         private static PooledArray<char> GetTexturePath(ReadOnlySpan<char> dir, ReadOnlyRawString name, out Span<char> texturePath, out ReadOnlySpan<char> ext)
         {
@@ -261,30 +238,6 @@ namespace Elffy.Serialization
             catch {
                 pooledArray.Dispose();
                 throw;
-            }
-        }
-
-        private struct ModelData : IDisposable
-        {
-            public UnsafeRawArray<SkinnedVertex> Vertices;
-            public UnsafeRawArray<Components.Bone> Bones;
-            public RefTypeRentMemory<IImageSource?> Images;
-
-            public ModelData(UnsafeRawArray<SkinnedVertex> vertices, UnsafeRawArray<Components.Bone> bones, RefTypeRentMemory<IImageSource?> images)
-            {
-                Vertices = vertices;
-                Bones = bones;
-                Images = images;
-            }
-
-            public void Dispose()
-            {
-                Vertices.Dispose();
-                Bones.Dispose();
-                foreach(var image in Images.AsSpan()) {
-                    image?.Dispose();
-                }
-                Images.Dispose();
             }
         }
     }
