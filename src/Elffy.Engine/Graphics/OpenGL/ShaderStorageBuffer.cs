ca06a215213729860e258a36a1bafd916e27786b
ikorin24 <sunshinexxab@yahoo.co.jp>
2022-02-17 01:59:41 +0900

    Add SSBO

---
+++ src/Elffy.Engine/Graphics/OpenGL/ShaderStorageBuffer.cs
@@ -0,0 +1,63 @@
+﻿#nullable enable
+using Elffy.Features;
+using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Elffy.Graphics.OpenGL
+{
+    public sealed class ShaderStorageBuffer : IDisposable
+    {
+        private Ssbo _ssbo;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private ShaderStorageBuffer(Ssbo ssbo)
+        {
+            _ssbo = ssbo;
+        }
+
+        ~ShaderStorageBuffer() => Dispose(false);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ShaderStorageBuffer Create()
+        {
+            var screen = Engine.GetValidCurrentContext();
+            var instance = new ShaderStorageBuffer(Ssbo.Create());
+            ContextAssociatedMemorySafety.Register(instance, screen);
+            return instance;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Bind() => Ssbo.Bind(_ssbo);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Unbind() => Ssbo.Unbind();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void BindBase(int index) => Ssbo.BindBase(_ssbo, index);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void BufferData<T>(ReadOnlySpan<T> data, BufferUsageHint usage) where T : unmanaged => Ssbo.BufferData(data, usage);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe void BufferDataUndefined(int byteSize, BufferUsageHint usage) => Ssbo.BufferData(byteSize, null, usage);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void Dispose(bool disposing)
+        {
+            if(disposing) {
+                Ssbo.Delete(ref _ssbo);
+            }
+            else {
+                ContextAssociatedMemorySafety.OnFinalized(this);
+            }
+        }
+    }
+}
---
+++ src/Elffy.Engine/Graphics/OpenGL/Ssbo.cs
@@ -0,0 +1,99 @@
+﻿#nullable enable
+using OpenTK.Graphics.OpenGL4;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Elffy.Graphics.OpenGL
+{
+    [DebuggerDisplay("{DebuggerView,nq}")]
+    public readonly struct Ssbo : IEquatable<Ssbo>
+    {
+        private readonly int _ssbo;
+
+        internal int Value => _ssbo;
+
+        public bool IsEmpty => _ssbo == Consts.NULL;
+
+        public static Ssbo Empty => default;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private string DebuggerView => IsEmpty ? "SSBO (null)" : $"SSBO={_ssbo}";
+
+        [Obsolete("Don't use defaut constructor.", true)]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public Ssbo() => throw new NotSupportedException("Don't use defaut constructor.");
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private Ssbo(int ssbo)
+        {
+            _ssbo = ssbo;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Ssbo Create()
+        {
+            GLAssert.EnsureContext();
+            return new Ssbo(GL.GenBuffer());
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Delete(ref Ssbo ssbo)
+        {
+            if(ssbo._ssbo != Consts.NULL) {
+                GLAssert.EnsureContext();
+                GL.DeleteBuffer(ssbo._ssbo);
+                ssbo = default;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Bind(Ssbo ssbo)
+        {
+            GLAssert.EnsureContext();
+            GL.BindBuffer(BufferTarget.ShaderStorageBuffer, ssbo._ssbo);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Unbind()
+        {
+            Bind(Empty);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void BufferData<T>(ReadOnlySpan<T> data, BufferUsageHint usage) where T : unmanaged
+        {
+            fixed(T* ptr = data) {
+                BufferData(data.Length * sizeof(T), ptr, usage);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void BufferData(int byteSize, void* data, BufferUsageHint usage)
+        {
+            GLAssert.EnsureContext();
+            GL.BufferData(BufferTarget.ShaderStorageBuffer, byteSize, (IntPtr)data, usage);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void BindBase(Ssbo ssbo, int index)
+        {
+            GLAssert.EnsureContext();
+            GL.BindBufferBase(BufferRangeTarget.ShaderStorageBuffer, index, ssbo._ssbo);
+        }
+
+        public override bool Equals(object? obj) => obj is Ssbo ssbo && Equals(ssbo);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(Ssbo other) => _ssbo == other._ssbo;
+
+        public override int GetHashCode() => _ssbo.GetHashCode();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(Ssbo left, Ssbo right) => left.Equals(right);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(Ssbo left, Ssbo right) => !(left == right);
+    }
+}
