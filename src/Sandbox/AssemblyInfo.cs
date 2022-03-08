87362bc3990072defad7b916f6d161084274a4aa
ikorin24 <sunshinexxab@yahoo.co.jp>
2022-03-09 03:08:43 +0900

    Remove EntryPointGenerator

--- src/Sandbox/AssemblyInfo.cs
+++ src/Sandbox/AssemblyInfo.cs
@@ -1,11 +1,5 @@
 ﻿#nullable enable
 using Elffy;
 
-//[assembly: GameLaunchSetting.GenerateMainMethod]
-[assembly: GameLaunchSetting.ScreenSize((int)(1200 * 1.5), (int)(675 * 1.5f))]
-[assembly: GameLaunchSetting.ScreenTitle("Sandbox")]
-[assembly: GameLaunchSetting.ScreenIcon("Sandbox", "icon.ico")]
-[assembly: GameLaunchSetting.WindowStyle(WindowStyle.Default)]
-[assembly: GameLaunchSetting.LaunchDevEnv]
 [assembly: GenerateLocalResource("Resources", "Sandbox.dat")]
 [assembly: DefineLocalResource("Sandbox", "Sandbox.dat")]
