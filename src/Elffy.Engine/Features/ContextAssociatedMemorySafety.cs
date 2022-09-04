2b0f0e8c2f6d8eb9ac7ce16e15704eab832aaddc
ikorin24 <sunshinexxab@yahoo.co.jp>
2022-09-05 00:29:16 +0900

    update

--- src/Elffy.Engine/Features/ContextAssociatedMemorySafety.cs
+++ src/Elffy.Engine/Features/ContextAssociatedMemorySafety.cs
@@ -18,6 +18,14 @@ namespace Elffy.Features
         /// <remarks>If you want to set the value, use <see cref="EngineSetting.EnableContextAssociatedMemorySafety"/>.</remarks>
         public static bool IsEnabled => EngineSetting.EnableContextAssociatedMemorySafety;
 
+        public static SafetyKey RegisterNonDisposable<T>(T resource, Action<T> disposeAction, IHostScreen associatedScreen) where T : class
+        {
+            if(!IsEnabled) { return SafetyKey.Empty; }
+            var d = new DisposableWrapper<T>(resource, disposeAction);
+            _dict.Add(d, associatedScreen);
+            return new SafetyKey(d);
+        }
+
         /// <summary>Register a resource</summary>
         /// <typeparam name="T">type of resource</typeparam>
         /// <param name="resource">resource object</param>
@@ -26,6 +34,14 @@ namespace Elffy.Features
         {
             if(!IsEnabled) { return; }
             _dict.Add(resource, associatedScreen);
+        }
+
+        public static void OnFinalized(SafetyKey safetyKey)
+        {
+            var disposable = safetyKey.Disposable;
+            if(disposable != null) {
+                OnFinalized(disposable);
+            }
         }
 
         /// <summary>Add specified <paramref name="resource"/> to the queue for safety disposing.</summary>
@@ -87,6 +103,37 @@ namespace Elffy.Features
                     }
                 }
             }
+        }
+
+        private sealed class DisposableWrapper<T> : IDisposable where T : class
+        {
+            private readonly T _obj;
+            private readonly Action<T> _disposeAction;
+
+            public DisposableWrapper(T obj, Action<T> disposeAction)
+            {
+                _obj = obj;
+                _disposeAction = disposeAction;
+            }
+
+            public void Dispose() => _disposeAction.Invoke(_obj);
+        }
+
+        public readonly struct SafetyKey : IEquatable<SafetyKey>
+        {
+            private readonly IDisposable? _disposable;
+
+            internal static SafetyKey Empty => default;
+
+            internal IDisposable? Disposable => _disposable;
+
+            internal SafetyKey(IDisposable disposable) => _disposable = disposable;
+
+            public override bool Equals(object? obj) => obj is SafetyKey @object && Equals(@object);
+
+            public bool Equals(SafetyKey other) => _disposable == other._disposable;
+
+            public override int GetHashCode() => _disposable?.GetHashCode() ?? 0;
         }
     }
 }
