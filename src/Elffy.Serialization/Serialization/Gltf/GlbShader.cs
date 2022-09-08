67c6e07eb03993ab88fbcffa5916deae6e0db09b
ikorin24 <sunshinexxab@yahoo.co.jp>
2022-09-09 05:59:35 +0900

    tmp

--- src/Elffy.Serialization/Serialization/Gltf/GlbShader.cs
+++ src/Elffy.Serialization/Serialization/Gltf/GlbShader.cs
@@ -1,63 +1,35 @@
 ﻿#nullable enable
-using Elffy.Components;
 using Elffy.Imaging;
 using Elffy.Shading;
 using Elffy.Graphics.OpenGL;
 using System;
 using System.Diagnostics.CodeAnalysis;
-using Elffy.Features;
-using Elffy.Features.Implementation;
 
 namespace Elffy.Serialization.Gltf;
 
 internal sealed class GlbShader : SingleTargetRenderingShader
 {
-    private bool _disposed = false;
-    private TextureCore _baseColorTex;
-    private TextureCore _normalTex;
-
-    private bool _isSafetyRegistered;
-    private ContextAssociatedMemorySafety.SafetyKey _safety;
+    private Texture? _baseColor;
+    private Texture? _normal;
 
     public GlbShader()
     {
     }
 
-    ~GlbShader()
-    {
-        if(_isSafetyRegistered) {
-            ContextAssociatedMemorySafety.OnFinalized(_safety);
-        }
-    }
-
     public void SetBaseColorTexture(ReadOnlyImageRef image, TextureConfig config)
     {
-        if(_disposed) {
-            ThrowAlreadyDisposed();
-        }
-        var screen = Engine.GetValidCurrentContext();
-        _baseColorTex = new TextureCore(config);
-        _baseColorTex.Load(image);
-
-        if(_isSafetyRegistered == false) {
-            _safety = ContextAssociatedMemorySafety.RegisterNonDisposable(this, static self => self.DisposeUnmanagedData(), screen);
-            _isSafetyRegistered = true;
-        }
+        var texture = new Texture(config);
+        texture.Load(image);
+        _baseColor?.Dispose();
+        _baseColor = texture;
     }
 
     public void SetNormalTexture(ReadOnlyImageRef image, TextureConfig config)
     {
-        if(_disposed) {
-            ThrowAlreadyDisposed();
-        }
-        var screen = Engine.GetValidCurrentContext();
-        _normalTex = new TextureCore(config);
-        _normalTex.Load(image);
-
-        if(_isSafetyRegistered == false) {
-            _safety = ContextAssociatedMemorySafety.RegisterNonDisposable(this, static self => self.DisposeUnmanagedData(), screen);
-            _isSafetyRegistered = true;
-        }
+        var texture = new Texture(config);
+        texture.Load(image);
+        _normal?.Dispose();
+        _normal = texture;
     }
 
     [DoesNotReturn]
@@ -73,25 +45,44 @@ internal sealed class GlbShader : SingleTargetRenderingShader
 
     protected override void OnRendering(ShaderDataDispatcher dispatcher, Renderable target, in Matrix4 model, in Matrix4 view, in Matrix4 projection)
     {
-        dispatcher.SendUniform("_mvp", projection * view * model);
-        dispatcher.SendUniformTexture2D("_baseColorTex", _baseColorTex.Texture, TextureUnitNumber.Unit0);
-        if(_normalTex.IsEmpty == false) {
-            dispatcher.SendUniformTexture2D("_normalTex", _normalTex.Texture, TextureUnitNumber.Unit1);
+        dispatcher.SendUniform("_model", model);
+        dispatcher.SendUniform("_view", view);
+        dispatcher.SendUniform("_projection", projection);
+
+        dispatcher.SendUniform("_hasBaseColorTex", TryGetTexture(_baseColor, out var baseColor));
+        dispatcher.SendUniformTexture2D("_baseColorTex", baseColor, TextureUnitNumber.Unit0);
+
+        dispatcher.SendUniform("_hasNormalTex", TryGetTexture(_normal, out var normal));
+        dispatcher.SendUniformTexture2D("_normalTex", normal, TextureUnitNumber.Unit1);
+
+        var screen = target.GetValidScreen();
+        var lights = screen.Lights.GetLights();
+        var light = lights.FirstOrDefault();
+        var (lpos, lcolor) = light switch
+        {
+            null => (default, Color4.Black),
+            _ => (light.Position, light.Color),
+        };
+        dispatcher.SendUniform("_lpos", lpos);
+        dispatcher.SendUniform("_lcolor", lcolor);
+    }
+
+    private static bool TryGetTexture(Texture? texture, out TextureObject to)
+    {
+        if(texture == null) {
+            to = TextureObject.Empty;
+            return false;
         }
-        dispatcher.SendUniform("_hasNormalTex", !_normalTex.IsEmpty);
+        to = texture.TextureObject;
+        return to.IsEmpty == false;
     }
 
     protected override void OnProgramDisposed()
     {
-        GC.SuppressFinalize(this);
-        DisposeUnmanagedData();
-    }
-
-    private void DisposeUnmanagedData()
-    {
-        _baseColorTex.Dispose();
-        _normalTex.Dispose();
-        _disposed = true;
+        _baseColor?.Dispose();
+        _baseColor = null;
+        _normal?.Dispose();
+        _normal = null;
     }
 
     protected override void OnTargetAttached(Renderable target) { }
@@ -102,38 +93,88 @@ internal sealed class GlbShader : SingleTargetRenderingShader
 
     private static ShaderSource GetForwardShader() => new()
     {
-        VertexShader = @"#version 410
+        VertexShader =
+@"#version 410
 in vec3 _pos;
 in vec2 _uv;
 in vec3 _normal;
 in vec3 _tangent;
-out vec2 _vUV;
-out vec3 _vNormal;
-uniform mat4 _mvp;
+out V2f
+{
+    vec3 pos;
+    vec2 uv;
+    vec3 normal;
+    mat3 tbn;   // camera space -> tangent space
+    vec3 ldirTan;   // light dir in tangent space
+} _v2f;
+uniform mat4 _model;
+uniform mat4 _view;
+uniform mat4 _projection;
+uniform vec4 _lpos;
 void main()
 {
-    _vUV = _uv;
-    _vNormal = _normal;
-    gl_Position = _mvp * vec4(_pos, 1.0);
+    _v2f.pos = _pos;
+    _v2f.uv = _uv;
+    _v2f.normal = _normal;
+    mat4 modelView = _view * _model;
+    vec3 bitangent = cross(_normal, _tangent);
+    mat3 mvMat3 = mat3(modelView);
+    _v2f.tbn = transpose(mat3(mvMat3 * _tangent, mvMat3 * bitangent, mvMat3 * _normal));
+
+    vec4 vposCam = modelView * vec4(_pos, 1.0);
+    gl_Position = _projection * vposCam;
+
+    if(_lpos.w <= 0.001) {
+        vec3 ldirCam = mat3(_view) * -normalize(_lpos.xyz);
+        _v2f.ldirTan = _v2f.tbn * ldirCam;
+    }
+    else {
+        vec4 lposCam = _view * vec4((_lpos.xyz / _lpos.w), 1.0);
+        vec3 ldirCam = normalize(vposCam.xyz / vposCam.w - lposCam.xyz / lposCam.w);
+        _v2f.ldirTan = _v2f.tbn * ldirCam;
+    }
 }
 ",
         FragmentShader =
 @"#version 410
-in vec2 _vUV;
-in vec3 _vNormal;
+in V2f
+{
+    vec3 pos;
+    vec2 uv;
+    vec3 normal;
+    mat3 tbn;
+    vec3 ldirTan;
+} _v2f;
+uniform vec4 _lcolor;
 uniform sampler2D _baseColorTex;
 uniform sampler2D _normalTex;
+uniform bool _hasBaseColorTex;
 uniform bool _hasNormalTex;
 out vec4 _fragColor;
+const vec3 NoColor = vec3(1, 0, 1);
+
+
+uniform mat4 _model;
+uniform mat4 _view;
+
 void main()
 {
-    vec3 result;
-    vec3 baseColor = texture(_baseColorTex, _vUV).rgb;
-    vec3 n = _hasNormalTex ? texture(_normalTex, _vUV).rgb * 2 - vec3(1, 1, 1) : _vNormal;
+    vec3 baseColor = _hasBaseColorTex ? texture(_baseColorTex, _v2f.uv).rgb : NoColor;
+    vec3 normalTan = texture(_normalTex, _v2f.uv).rgb * 2 - vec3(1, 1, 1);
 
-    //result = n;
-    result = texture(_normalTex, _vUV).rgb * 2.0 - vec3(1.0, 1.0, 1.0);
-    result = baseColor;
+
+    const float afactor = 0.8;
+    const float dfactor = 0.35;
+    const float sfactor = 0.2;
+    const float shininess = 1;
+    float dotNL = max(0, dot(normalTan, -_v2f.ldirTan));
+    vec3 ambient = afactor * baseColor;
+    vec3 diffuse = dotNL * dfactor * baseColor;
+    vec3 rTan = reflect(_v2f.ldirTan, normalTan);
+
+    vec3 V = _v2f.tbn * -(_view * _model * vec4(_v2f.pos, 1)).xyz;
+    vec3 specular = max(pow(max(0.0, dot(rTan, V)), shininess), 0.0) * sfactor * baseColor;
+    vec3 result = (ambient + diffuse + specular) * _lcolor.rgb;
     _fragColor = vec4(result, 1);
 }
 ",
